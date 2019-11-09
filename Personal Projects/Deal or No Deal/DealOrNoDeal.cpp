9ae9fdd84606e8f113ca9e0b39ae16df10fb0d5f
FieldMarshallObvious <noahdelangel@gmail.com>
2019-11-09 12:45:06 -0600

    Added logic for selecting cases, and the string generation of cases almosts works

--- Personal Projects/Deal or No Deal/DealOrNoDeal.cpp
+++ Personal Projects/Deal or No Deal/DealOrNoDeal.cpp
@@ -8,118 +8,152 @@
 #include <iostream>
 #include <iomanip>
 #include <cstdlib>
+#include <string>
 #include <time.h>
 
 const int CASE_AMOUNT = 6;
 const int RAND_PLACE_HOLDER = 1;
 
-void pickCase ( );
-void makeCases ( int (&casesList)[CASE_AMOUNT][RAND_PLACE_HOLDER] );
-void drawCases (char (&casesOpened)[CASE_AMOUNT],
+void pickCase ( char (&casesOpened)[CASE_AMOUNT],
                 int (&casesList)[CASE_AMOUNT][RAND_PLACE_HOLDER]);
-void drawCase ( int index, int difference );
-void drawCase ( int (&casesList)[CASE_AMOUNT][RAND_PLACE_HOLDER], int index, int difference );
-bool inputCaseCheck ( );
+void createCases ( int (&casesList)[CASE_AMOUNT][RAND_PLACE_HOLDER],
+                   std::string (&casesPrint)[CASE_AMOUNT/2][CASE_AMOUNT/3] );
+void drawCases ( std::string (&casesPrint)[CASE_AMOUNT/2][CASE_AMOUNT/3] );
+
+std::string drawCase ( int index, int difference );
+std::string drawCase ( int (&casesList)[CASE_AMOUNT][RAND_PLACE_HOLDER], int index, int difference );
+
+bool inputCaseCheck ( char (&casesOpened)[CASE_AMOUNT], int input );
+
 int ranNumGen ( );
-
-
 
 using namespace std;
 
 int main ( )
 {
+    string casesPrint[CASE_AMOUNT/2][CASE_AMOUNT/3];
     char casesOpened[CASE_AMOUNT];
     
     int casesList[CASE_AMOUNT][RAND_PLACE_HOLDER];
     
-    makeCases(casesList);
-    drawCases(casesOpened, casesList);
+    createCases(casesList, casesPrint );
+    drawCases(casesPrint);
 }
 
 void pickCase ( char (&casesOpened)[CASE_AMOUNT],
                 int (&casesList)[CASE_AMOUNT][RAND_PLACE_HOLDER] )
 {
+    int input = -1;
+    bool checker = false;
+
+    while ( checker != true )
+    {
+        cout << "Please pick a case from the selection";
+        cin >> input;
+        
+        checker = inputCaseCheck ( casesOpened, input );
+        
+    }
+    
     
 }
 
-void makeCases ( int (&casesList)[CASE_AMOUNT][RAND_PLACE_HOLDER] )
+void createCases ( int (&casesList)[CASE_AMOUNT][RAND_PLACE_HOLDER],
+                   std::string (&casesPrint)[CASE_AMOUNT/2][CASE_AMOUNT/3] )
 {
-    for ( int index = 0; index < CASE_AMOUNT; index++)
+    
+    bool passed = false;
+    
+    int difference = 0;
+    
+    for ( int index = 0; index < CASE_AMOUNT; index++ )
     {
         casesList[index][0] = ranNumGen( );
     }
-}
-
-
-void drawCases ( char (&casesOpened)[CASE_AMOUNT],
-                 int (&casesList)[CASE_AMOUNT][RAND_PLACE_HOLDER] )
-{
-    int difference = 0;
-    bool passed = false;
     
-
-    
-    for( int index = 0; index < CASE_AMOUNT; index++)
+    for ( int row = 0; row < CASE_AMOUNT/2; row++)
     {
-        
         if ( passed == true )
+            difference = CASE_AMOUNT/3;
+        for ( int col = 0; col < CASE_AMOUNT/3; col++ )
         {
-            difference = 3;
-        }
-        
-        if ( casesOpened[index] == 'U')
-        {
-            if ( passed == true )
-                passed = false;
-            
-            drawCase( index, difference );
-        
-            
-        }
-        else if (!(casesOpened[index] == 'y'))
-        {
-            if ( passed == true )
-                passed = false;
 
-            drawCase( index, difference );
+            casesPrint[row][col] = drawCase(col, difference);
+            
+           
         }
-        else
-        {
-            if ( passed == true )
-                passed = false;
-           drawCase( casesList, index, difference );
-        }
-        
-        if ( passed == false)
-            difference = 0;
-    
+        passed = true;
     }
 }
 
-void drawCase ( int index, int difference )
+
+
+void drawCases ( std::string (&casesPrint)[CASE_AMOUNT/2][CASE_AMOUNT/3] )
 {
-    cout << " .---[[__]]----. " << endl
-    << ";-------------.|" << endl
-    << "|             ||" << endl
-    << "|             ||" << endl
-    << "|      " << index + difference+1 << "      ||" << endl
-    << "|             ||" << endl
-    << "|_____________|/" << endl;
+    
+
+    
+    for ( int row = 0; row < CASE_AMOUNT/2; row++)
+    {
+ 
+        for ( int col = 0; col < CASE_AMOUNT/3; col++ )
+        {
+            cout <<  casesPrint[row][col];
+            for ( int reverse = 0; reverse < 7; reverse++ )
+                cout << endl;
+                cout << "                  " <<"\x1b[A";
+        }
+        
+        cout << endl;
+    }
 }
-void drawCase ( int (&casesList)[CASE_AMOUNT][RAND_PLACE_HOLDER], int index, int difference )
+
+std::string drawCase ( int index, int difference )
 {
-    cout << " .---[[__]]----. " << endl
-    << ";-------------.|" << endl
-    << "|              |" << endl
-    << "|              |" << endl
-    << "|       $" << casesList[index+difference][0] << "      ||" << endl
-    << "|              |" << endl
-    << "|______________/" << endl
-    << "/             /" << endl
-    << "/            / " << endl
-    << "/           /" << endl
-    << "/          /" << endl
-    << "___________" << endl;
+    string output =  " .---[[__]]----. " + string("\n")
+    + ";-------------.|" + "\n"
+    + "|             ||" + "\n"
+    + "|             ||" + "\n"
+    + "|     " + std::to_string(index + difference) + "       ||" + "\n"
+    + "|             ||" + "\n"
+    + "|_____________|/" + "\n";
+    
+    return output;
+}
+std::string drawCase ( int (&casesList)[CASE_AMOUNT][RAND_PLACE_HOLDER], int index, int difference )
+{
+    string output =  " .---[[__]]----. " + string("\n")
+    + ";-------------.|" + "\n"
+    + "|              |" + "\n"
+    + "|              |" + "\n"
+    + "|       $" + std::to_string(casesList[index+difference][0]) + "      ||" + "\n"
+    + "|              |" + "\n"
+    + "|______________/" + "\n"
+    + "/             /" + "\n"
+    + "/            / " + "\n"
+    + "/           /" + "\n"
+    + "/          /" + "\n"
+    + "___________" + "\n";
+    
+    return output;
+}
+
+bool inputCaseCheck ( char (&casesOpened)[CASE_AMOUNT], int input )
+{
+    bool output = false;
+    
+    if (input <= 0 || input >= CASE_AMOUNT )
+        cout << "Please enter a value between 1 and " << CASE_AMOUNT;
+    else if ( casesOpened[input - 1] == 'u' )
+        cout << "You already chose this case, please select a different one.";
+    else if ( casesOpened[input-1] == 'U' )
+        cout << "This case has already been opened, choose a different one.";
+    else
+        output = true;
+    
+    return output;
+        
+    
 }
 
 int ranNumGen  ( )
