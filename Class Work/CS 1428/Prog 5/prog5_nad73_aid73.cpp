1dbf4ab8aaa909881481d76b649e7d37df9b92c4
FieldMarshallObvious <noahdelangel@gmail.com>
2019-11-11 19:39:34 -0600

    Fixed ostream to ifstream conversion. Then refined output statements

--- Class Work/CS 1428/Prog 5/prog5_nad73_aid73.cpp
+++ Class Work/CS 1428/Prog 5/prog5_nad73_aid73.cpp
@@ -1,21 +1,21 @@
 /*
  File: prog5_bb0000.cpp  // Remove this inline comment after replacing 'bb0000' with your account number.
- 
+
  Author: Betty Boop      // Remove this inline comment after replacing Betty Boop with your first & last names.
  C.S.1428.?              // Remove this inline comment after replacing the '?' with your three-digit lecture section number.
  Lab Section: L?         // Remove this inline comment after replacing the '?' with your two-digit lab section number.
  Program: #5
  Due Date: --/--/--      // Remove this inline comment after replacing the dashes with the due date, in this format - month, day, year.
- 
+
  This program reads an unknown number of simple binary expressions
  of the form 'operand1 operator operand2' from an input file. Each
  expression is evaluated. Real input values are allowed.
- 
+
  A sentinel value read loop is used to process as described below:
- 
+
  (Named constants to 'hold' CLASS ("C.S.1428") and DUE_DATE ("--/--/--")
  are declared in the calling routine (main).)
- 
+
  - A void function, getName, is called to 'get' the team members' first & last
  names using two different prompts for each team member leaving a blank
  line between sets of prompts. Four strings, one to 'hold' the first name
@@ -53,43 +53,43 @@
  message to the screen indicating to the user the name of the output
  file to which the results have been written. (Refer to sample output
  below.)
- 
+
  ==========================================================================
- 
+
  *The expressions are checked for the addition (+), subtraction (-),
  multiplication (*), and division (/) operators. Included are a check for
  division by zero when the division operator is encountered and a check for
  an invalid operator (e.g. 134.6 ! 23.1). Since these are mutually exclusive
  events, a switch statement with a nested double-sided alternative for
  handling the division operator was used to evaluate the expressions.
- 
+
  If division by zero is encountered, the original expression is echoed to
  the output file along with the message '   Division by zero produces an
  undefined result.' If an invalid operator is encountered, the original
  expression is echoed to the output file along with the message
  '   Encountered unknown operator.'
- 
+
  Every attempt is made to eliminate repetitive code!
- 
+
  Numeric values in output are NOT formatted!
- 
+
  ==========================================================================
- 
+
  The layout and content of the file input and the file output are shown in
  the samples below. An appropriate message is displayed to the screen if
  either the input file or the output file fails to open.
- 
+
  Input (file - prog5_?inp.txt):  // '?' represents three-digit lecture number
  (contains an unknown number of binary expressions)
- 
+
  operand1(double)  operator(char)  operand2(double)
  ...
- 
+
  Constants: CLASS = "C.S.1428"    (string)
  DUE_DATE = "--/--/--" (string)
- 
+
  Sample Screen Display:
- 
+
  Enter your first name:
  Enter your last name:
  <blank line>
@@ -108,9 +108,9 @@
  --/--/--             // dashes represent due date, month/day/year
  <blank line>
  <Appropriate message indicating the name of the output file.>
- 
+
  Sample File Output (prog5_?out.txt): //'?' represents three-digit lecture sec #
- 
+
  Authors' Names
  C.S.1428.?             // '?' represents three-digit lecture section number
  Lab Section: L? & L?   // '?' represents two-digit lab section number
@@ -120,9 +120,9 @@
  123.5 + 59.3 = 182.8
  198.7 / 0   Division by zero produces an undefined result.
  ...
- 
+
  ================================================
- 
+
  <Output will vary based on actual input values.>
  */
 
@@ -134,22 +134,27 @@ using namespace std;
 
 void getName ( string &first1, string &last1, string &first2, string &last2 );
 void printIdInfo ( );
-void printIdInfo ( ostream &output_file, string first1, string last1, string first2, string last2,                 const string CLASS, string section, string lab_section1, string lab_section2,
+void printIdInfo ( ostream &output_file, string first1, string last1,
+                  string first2, string last2, const string CLASS,
+                  string section, string lab_section1, string lab_section2,
                    const string DUE_DATE );
-void readExpression ( ostream &input_file, double &operand1, char &operatr, double &operand2)
-void echoExpression ( ostream &output_file, double operand1, char operatr, double operand2 );
-void evaluateExpression ( ostream &output_file, double operand1, char operatr, double operand2 );
+void readExpression ( ifstream &input_file, double &operand1, char &operatr,
+                     double &operand2);
+void echoExpression ( ostream &output_file, double operand1, char operatr,
+                     double operand2 );
+void evaluateExpression ( ostream &output_file, double operand1, char operatr,
+                         double operand2 );
 void writeFileLocation ( );
 
 
 string getLectureSection ( );
-string getLabSection ( string section1, string section2 );
+string getLabSection ( string &section1, string &section2 );
 
 int main ( )
 {
     const string CLASS = "C.S.1428",
     DUE_DATE = "11/13/19";
-    
+
     string first1_name,
     last1_name,
     first2_name,
@@ -157,33 +162,33 @@ int main ( )
     lecture_section,
     lab_section1,
     lab_section2;
-    
+
     double operand1,
     operand2;
-    
-    char   operatr;
-    
+
+    char operatr;
+
     ifstream fin;
-    fin.open("...");   // REMOVE THIS COMMENT AFTER REPLACING ... WITH THE NAME OF THE INPUT FILE.
-    
+    fin.open("prog5_001inp.txt");
+
     // REPLACE THIS COMMENT WITH CODE TO VERIFY THE INPUT FILE OPENS.
     // USE THE CODE FROM THE EXAMPLE PROVIDED ON THE ASSIGNMENTS PAGE.
     // ONLY MAKE NECESSARY ADJUSTMENTS, OR POINTS WILL BE DEDUCTED.
-    
+
     ofstream fout;
-    fout.open("...");  // REMOVE THIS COMMENT AFTER REPLACING ... WITH THE NAME OF THE OUTPUT FILE.
-    
+    fout.open("prog5_001out.txt");
+
     // REPLACE THIS COMMENT WITH CODE TO VERIFY THE OUTPUT FILE OPENS.
     // USE THE CODE FROM THE EXAMPLE PROVIDED ON THE ASSIGNMENTS PAGE.
     // ONLY MAKE NECESSARY ADJUSTMENTS, OR POINTS WILL BE DEDUCTED.
-    
+
     getName ( first1_name, last1_name, first2_name, last2_name );
     lecture_section = getLectureSection ( );
     getLabSection ( lab_section1, lab_section2 );
     printIdInfo ( fout, first1_name, last1_name, first2_name, last2_name,
                  CLASS, lecture_section,
                  lab_section1, lab_section2, DUE_DATE );
-    
+
     readExpression ( fin, operand1, operatr, operand2 );
     while ( operatr != '?' )
     {
@@ -191,27 +196,27 @@ int main ( )
         evaluateExpression ( fout, operand1, operatr, operand2 );
         readExpression ( fin, operand1, operatr, operand2 );
     }
-    
+
     cout << endl << endl;
     printIdInfo ( cout, first1_name, last1_name, first2_name, last2_name,
                  CLASS, lecture_section, lab_section1, lab_section2, DUE_DATE );
     writeFileLocation ( );
-    
+
     fin.close();
     fout.close();
-    
+
     system("PAUSE>NUL");
-    
+
     return 0;
 }
 
 /*
  Function: getName
- 
+
  The function, getName, uses two separate prompts to 'get' the user's first
  and last names, leaves one blank line then repeats the process to obtain
  the first and last names of the user's team member.
- 
+
  Sample Screen Display:
  Enter your first name:
  Enter your last name:
@@ -219,7 +224,7 @@ int main ( )
  Enter your team member's first name:
  Enter your team member's last name:
  <blank line>
- 
+
  Receives: first1, last1, first2, last2 (strings); in this order
  Constants: none
  Returns: first & last names of team members
@@ -228,35 +233,35 @@ void getName ( string &first1, string &last1, string &first2, string &last2 )
 {
     cout << "Enter your first name: ";
     cin >> first1;
-    
+
     cout << endl;
-    
+
     cout << "Enter your last name: ";
     cin >> last1;
-    
+
     cout << endl;
-    
+
     cout << "Enter your team member's first name: ";
     cin >> first2;
-    
+
     cout << endl;
-    
+
     cout << "Enter your team member's last name: ";
     cin >> last2;
-    
+
     cout << endl << endl;
 }
 
 /*
  Function: getLectureSection
- 
+
  The function, getLectureSection, prompts the user for the three-digit
  lecture section number and returns it to the calling routine.
- 
+
  Sample Screen Display:
  Enter your three-digit lecture section number:
  <blank line>
- 
+
  Receives: nothing
  Constants: none
  Returns: lecture section number
@@ -265,43 +270,43 @@ void getName ( string &first1, string &last1, string &first2, string &last2 )
 string getLectureSection ( )
 {
     string sectionInput;
-    
+
     cout << "Enter your three-digit lecture section number: ";
     cin >> sectionInput;
-    
+
     return sectionInput;
 }
 
 /*
  Function: getLabSection
- 
+
  The function, getLabSection, prompts the user for his/her two-digit lab
  section number. It then prompts for the two-digit lab section number of
  the user's team member.
- 
+
  Sample Screen Display:
  Enter your two-digit lab section number:
  Enter your team member's two-digit lab section number:
- 
+
  Receives: section1, section2 (string); in this order
  Constants: none
  Returns: team members' lab section numbers
  */
 
-void getLabSection ( string &section1, string &section2 )
+string getLabSection ( string &section1, string &section2 )
 {
     cout << "Enter your two-digit lab section number: ";
     cin >> section1;
-    
+
     cout << endl << endl;
-    
+
     cout << "Enter your team member's two digit lab section number: ";
     cin >> section2;
 }
 
 /*
  Function: printIdInfo
- 
+
  The function, printIdInfo, writes the team members' identifying
  information to the output file on four separate lines
  - firstname1 lastname1 & firstname2 lastname2
@@ -309,14 +314,14 @@ void getLabSection ( string &section1, string &section2 )
  - "Lab Section: L"labsection1" & L"labsection2
  - program due date.
  One blank line is left after the due date.
- 
+
  Sample Output:
  Authors' Names
  C.S.1428.?             // '?' represents three-digit lecture section #
  Lab Section: L? & L?   // '?' represents two-digit lab section #s
  --/--/--               // dashes represent due date, month/day/year
  <blank line>
- 
+
  Receives: output file variable,
  first1, last1, first2, last2, (strings)
  CLASS (string constant), section (string),
@@ -326,21 +331,25 @@ void getLabSection ( string &section1, string &section2 )
  Returns: nothing; prints user's team identifying info to a file
  */
 
-void printIdInfo ( ostream &output_file, string first1, string last1, string first2, string last2, const string CLASS,                   string section, string lab_section1, string lab_section2,
+void printIdInfo ( ostream &output_file, string first1, string last1,
+                  string first2, string last2, const string CLASS,
+                  string section, string lab_section1, string lab_section2,
                    const string DUE_DATE )
 {
-    output_file << first1 << " " << last1 << " & " << first2 <<  last2 << endl
-         << CLASS << "." << section << endl
-         << "Lab Section: " << "L" << lab_section1 << " & " << "L" << lab_section2 << endl
-    << DUE_DATE << endl;
+    output_file << first1 << " " << last1 << " & " << first2 << " " << last2
+                << endl
+                << CLASS << "." << section << endl
+                << "Lab Section: " << "L" << lab_section1 << " & " << "L"
+                << lab_section2 << endl
+                << DUE_DATE << endl << endl;
 }
 
 /*
  Function: readExpression
- 
+
  The function, readExpression, uses one statement to read a binary
  expression of the form operand1 operator operand2 from an input file
- 
+
  Receives: input file variable,
  operand1 (double), operatr (char), operand2 (double);
  in this order
@@ -348,21 +357,22 @@ void printIdInfo ( ostream &output_file, string first1, string last1, string fir
  Returns: operand1 (double), operatr (char), operand2 (double)
  */
 
-void readExpression ( ostream &input_file, double &operand1, char &operatr, double &operand2)
+void readExpression ( ifstream &input_file, double &operand1, char &operatr,
+                      double &operand2)
 {
     input_file >> operand1 >> operatr >> operand2;
 }
 
 /*
  Function: echoExpression
- 
+
  The function, echoExpression, echoes the binary expression to an output
  file.
- 
+
  Sample Output:
  123.5 + 59.3
  ...
- 
+
  Receives: output file variable,
  operand1 (double), operatr (char), operand2 (double);
  in this order
@@ -370,25 +380,26 @@ void readExpression ( ostream &input_file, double &operand1, char &operatr, doub
  Returns: nothing; writes expression to a file
  */
 
-void echoExpression ( ostream &output_file, double operand1, char operatr, double operand2 )
+void echoExpression ( ostream &output_file, double operand1, char operatr,
+                      double operand2 )
 {
     output_file << operand1 << " " << operatr << " " << operand2 << " ";
 }
 
 /*
  Function: evaluateExpression
- 
+
  The function, evaluateExpression, evaluates the expression to determine
  whether or not it is valid. Valid expressions are evaluated and the
  results written to a file; otherwise, an appropriate message
  is written to the file.
- 
+
  Sample Output:
  125 $ 28   Encountered unknown operator.
  123.5 + 59.3 = 182.8
  198.7 / 0   Division by zero produces an undefined result.
  ...
- 
+
  Receives: output file variable,
  operand1 (double), operatr (char), operand2 (double);
  in this order
@@ -396,7 +407,8 @@ void echoExpression ( ostream &output_file, double operand1, char operatr, doubl
  Returns: nothing; writes results to a file
  */
 
-void evaluateExpression ( ostream &output_file, double operand1, char operatr, double operand2 )
+void evaluateExpression ( ostream &output_file, double operand1, char operatr,
+                          double operand2 )
 {
     switch ( operatr )
     {
@@ -418,19 +430,21 @@ void evaluateExpression ( ostream &output_file, double operand1, char operatr, d
         default:
             output_file << "Encountered unknown operator.";
     }
+
+    output_file << endl;
 }
 
 /*
  Function: writeFileLocation
- 
+
  The function, writeFileLocation, writes a message to the screen
  indicating to the user the name of the output file to which the
  results have been written.
- 
+
  Sample Display:
  Program results have been written to prog5_00?out.txt.
  (The actual file name may differ.)
- 
+
  Receives: nothing
  Constants: none
  Returns: nothing; writes a message to the screen
