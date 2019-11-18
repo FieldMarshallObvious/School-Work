35a73e174c33e26b2f8d0b0f8133cbf16b968c36
FieldMarshallObvious <noahdelangel@gmail.com>
2019-11-18 12:00:44 -0600

    attemped fixing random selection - eliminated infinite loop

--- Personal Projects/Deal or No Deal/DealOrNoDeal.cpp
+++ Personal Projects/Deal or No Deal/DealOrNoDeal.cpp
@@ -35,13 +35,12 @@ int main ( )
     string casesPrint[CASE_AMOUNT/2][2];
     char casesOpened[CASE_AMOUNT];
     
-    int casesList[CASE_AMOUNT][RAND_PLACE_HOLDER],
-        possibleChoices[CASE_AMOUNT];
+    int casesList[CASE_AMOUNT][RAND_PLACE_HOLDER];
     
-    double randomizeDollar[26] =
-    {.01, 5.0, 25.0, 75.0, 200.0, 400.0, 750.0, 5000.0, 25000.0, 75000.0, 200000.0,
-        400000.0, 750000.0, 1.0, 10.0, 50.0, 100.0, 300.0, 500.0, 1000.0, 10000.0,
-        50000.0, 100000.0, 300000.0, 500000.0, 1000000.0};
+    int possibleChoices[26] =
+    {1, 5, 25, 75 , 200 , 400 , 750 , 5000 , 25000 , 75000 , 200000 ,
+        400000 , 750000 , 1 , 10 , 50 , 100 , 300 , 500 , 1000 , 10000 ,
+        50000 , 100000 , 300000 , 500000 , 1000000 };
     
     createCases( possibleChoices, casesList, casesPrint );
     drawCases(casesPrint);
@@ -74,17 +73,21 @@ void createCases ( int possibleChoices[] ,int
     
     int difference = 0;
     
+    cout << "Entering case list loop" << endl;
     for ( int index = 0; index < CASE_AMOUNT; index++ )
     {
+        cout << "Creating random number for current index" << endl;
         casesList[index][0] = ranNumGen( possibleChoices, casesList );
     }
     
+    cout << "Entering case amount..." << endl;
     for ( int row = 0; row < CASE_AMOUNT/2; row++)
     {
         if ( passed == true )
             difference = CASE_AMOUNT/3;
-        for ( int col = 0; col < CASE_AMOUNT/3; col++ )
+        for ( int col = 0; col < 2; col++ )
         {
+            cout << "Bruh";
             casesPrint[row][col] = drawCase(col, difference);
         }
         passed = true;
@@ -112,7 +115,7 @@ void drawCases ( std::string (&casesPrint)[CASE_AMOUNT/2][2] )
 }
 
 std::string drawCase ( int index, int difference )
-{\
+{
     string output =  " [ " +  std::to_string(index + difference) + " ]";
     
     return output;
@@ -144,18 +147,26 @@ bool inputCaseCheck ( char (&casesOpened)[CASE_AMOUNT], int input )
 
 int ranNumGen  ( int possibleChoices[], int (&casesList)[CASE_AMOUNT][RAND_PLACE_HOLDER] )
 {
-    int randnum = 1;
-    
+    int randnum;
+
     bool repeat = true;
+    
+    srand(time(NULL));
     
     while ( repeat == true )
     {
-        srand (time(NULL));
         randnum = rand() % 25;
+        
+        cout << "The random number is " << possibleChoices[randnum] << endl;
+        cout << "The random index is " << randnum << endl;
+        
         for ( int index = 0; index < CASE_AMOUNT; index++ )
         {
+            
+
             if ( possibleChoices[randnum] == casesList[index][0])
             {
+                cout << "NEED NEW NUMBER" << endl;
                 repeat = true;
                 break;
             }
