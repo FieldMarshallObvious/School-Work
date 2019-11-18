30b2cf4c0cc59e95c53d73ceff10ab0eca8f61b5
FieldMarshallObvious <noahdelangel@gmail.com>
2019-11-17 19:10:44 -0600

    Rewrote the string logic - and add a possibilites array

--- Personal Projects/Deal or No Deal/DealOrNoDeal.cpp
+++ Personal Projects/Deal or No Deal/DealOrNoDeal.cpp
@@ -11,15 +11,15 @@
 #include <string>
 #include <time.h>
 
-const int CASE_AMOUNT = 6;
+const int CASE_AMOUNT = 26;
 const int RAND_PLACE_HOLDER = 1;
 
 void pickCase ( char (&casesOpened)[CASE_AMOUNT],
                 int (&casesList)[CASE_AMOUNT][RAND_PLACE_HOLDER]);
 void createCases ( int possibleChoices[] ,int
                   (&casesList)[CASE_AMOUNT][RAND_PLACE_HOLDER],
-                  std::string (&casesPrint)[CASE_AMOUNT/2][CASE_AMOUNT/3] );
-void drawCases ( std::string (&casesPrint)[CASE_AMOUNT/2][CASE_AMOUNT/3] );
+                  std::string (&casesPrint)[CASE_AMOUNT/2][2] );
+void drawCases ( std::string (&casesPrint)[CASE_AMOUNT/2][2] );
 
 std::string drawCase ( int index, int difference );
 std::string drawCase ( int (&casesList)[CASE_AMOUNT][RAND_PLACE_HOLDER], int index, int difference );
@@ -32,11 +32,16 @@ using namespace std;
 
 int main ( )
 {
-    string casesPrint[CASE_AMOUNT/2][CASE_AMOUNT/3];
+    string casesPrint[CASE_AMOUNT/2][2];
     char casesOpened[CASE_AMOUNT];
     
     int casesList[CASE_AMOUNT][RAND_PLACE_HOLDER],
         possibleChoices[CASE_AMOUNT];
+    
+    double randomizeDollar[26] =
+    {.01, 5.0, 25.0, 75.0, 200.0, 400.0, 750.0, 5000.0, 25000.0, 75000.0, 200000.0,
+        400000.0, 750000.0, 1.0, 10.0, 50.0, 100.0, 300.0, 500.0, 1000.0, 10000.0,
+        50000.0, 100000.0, 300000.0, 500000.0, 1000000.0};
     
     createCases( possibleChoices, casesList, casesPrint );
     drawCases(casesPrint);
@@ -62,7 +67,7 @@ void pickCase ( char (&casesOpened)[CASE_AMOUNT],
 
 void createCases ( int possibleChoices[] ,int
                   (&casesList)[CASE_AMOUNT][RAND_PLACE_HOLDER],
-                  std::string (&casesPrint)[CASE_AMOUNT/2][CASE_AMOUNT/3] )
+                  std::string (&casesPrint)[CASE_AMOUNT/2][2] )
 {
     
     bool passed = false;
@@ -80,10 +85,7 @@ void createCases ( int possibleChoices[] ,int
             difference = CASE_AMOUNT/3;
         for ( int col = 0; col < CASE_AMOUNT/3; col++ )
         {
-
             casesPrint[row][col] = drawCase(col, difference);
-            
-           
         }
         passed = true;
     }
@@ -91,7 +93,7 @@ void createCases ( int possibleChoices[] ,int
 
 
 
-void drawCases ( std::string (&casesPrint)[CASE_AMOUNT/2][CASE_AMOUNT/3] )
+void drawCases ( std::string (&casesPrint)[CASE_AMOUNT/2][2] )
 {
     
 
@@ -102,9 +104,7 @@ void drawCases ( std::string (&casesPrint)[CASE_AMOUNT/2][CASE_AMOUNT/3] )
         for ( int col = 0; col < CASE_AMOUNT/3; col++ )
         {
             cout <<  casesPrint[row][col];
-            for ( int reverse = 0; reverse < 7; reverse++ )
-                cout << endl;
-                cout << "                  " <<"\x1b[A";
+
         }
         
         cout << endl;
@@ -112,31 +112,14 @@ void drawCases ( std::string (&casesPrint)[CASE_AMOUNT/2][CASE_AMOUNT/3] )
 }
 
 std::string drawCase ( int index, int difference )
-{
-    string output =  " .---[[__]]----. " + string("\n")
-    + ";-------------.|" + "\n"
-    + "|             ||" + "\n"
-    + "|             ||" + "\n"
-    + "|     " + std::to_string(index + difference) + "       ||" + "\n"
-    + "|             ||" + "\n"
-    + "|_____________|/" + "\n";
+{\
+    string output =  " [ " +  std::to_string(index + difference) + " ]";
     
     return output;
 }
 std::string drawCase ( int (&casesList)[CASE_AMOUNT][RAND_PLACE_HOLDER], int index, int difference )
 {
-    string output =  " .---[[__]]----. " + string("\n")
-    + ";-------------.|" + "\n"
-    + "|              |" + "\n"
-    + "|              |" + "\n"
-    + "|       $" + std::to_string(casesList[index+difference][0]) + "      ||" + "\n"
-    + "|              |" + "\n"
-    + "|______________/" + "\n"
-    + "/             /" + "\n"
-    + "/            / " + "\n"
-    + "/           /" + "\n"
-    + "/          /" + "\n"
-    + "___________" + "\n";
+    string output =   " [ " +  std::to_string(index+difference)  + ": " + std::to_string(casesList[index+difference][0]) +" ]";
     
     return output;
 }
