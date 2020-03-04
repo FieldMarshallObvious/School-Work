bcbbb25044d6731eb9e12c0b4c218978c7c99881
FieldMarshallObvious <noahdelangel@gmail.com>
2020-03-04 15:30:59 -0600

    Added logic to count number of songs, check if input was a number. Also added logic to added song to an array. Changed Read_songs to Songs_array_mod, to make a overloaded method for removing songs

--- Class Work/CS 2308/prog 2/noah_delangel_prog2.cpp
+++ Class Work/CS 2308/prog 2/noah_delangel_prog2.cpp
@@ -25,6 +25,7 @@
 
 using namespace std;
 
+double MAX_MEMORY = 25;
 
 struct Song
 {
@@ -37,13 +38,14 @@ struct Song
 
 int Count_songs ( ifstream &input_songs );
 void Cleanup ( Song playlist[] );
+bool Is_number( const string input);
 int Rand_num_generator ( int chosen_songs[], int seed );
 string Read_lines ( ifstream &input_songs );
-Song * Read_songs ( ifstream &input_songs );
 void Remove_a_song( Song playlist[] );
 void Run_ipod( Song playlist[], ifstream &input_songs );
 void Show_playlist( Song playlist[] );
 void Shuffle( Song playlist[] );
+Song * Songs_array_mod ( ifstream &input_songs );
 void Exit( Song playlist[] );
 
 /*=====================================================================
@@ -54,7 +56,63 @@ void Exit( Song playlist[] );
 
 int main ( )
 {
+    ifstream input_songs;
+    
+    input_songs.open( "input_song.txt" );
+    
+    //Checks if question bank file succesfully opened
+    if( ! input_songs )
+    {
+        cout << "***Program Terminated.***" << endl << endl
+        << "Songs file failed to open." << endl;
+        
+        return -1;
+    }
+    
+    Songs_array_mod ( input_songs );
+    
+    input_songs.close();
+    
     return 0;
+}
+
+/*=====================================================================
+ Function: Count_songs
+ Description:
+ Parameters: ifstream input_songs
+ ======================================================================*/
+int Count_songs ( ifstream &input_songs )
+{
+    string cur_line;
+    
+    double mem_used,
+           cntr = 0,
+           num_songs = 0;
+    
+    while( mem_used <= MAX_MEMORY )
+    {
+        cntr++;
+        cur_line = Read_lines( input_songs );
+        
+        
+        if ( cur_line == " " )
+        {
+            break;
+        }
+        
+        if( cntr == 3 )
+        {
+            if ( ( mem_used + stod( cur_line.c_str( ) ) ) <= MAX_MEMORY )
+            {
+                cntr = 0;
+                num_songs++;
+                mem_used+= stod( cur_line.c_str( ) );
+            }
+        }
+    }
+    
+    return num_songs;
+    
 }
 
 /*=====================================================================
@@ -66,6 +124,23 @@ void Cleanup( Song playlist[] )
 {
     //Clears up dynamic memory space
     delete [] playlist;
+}
+
+/*=====================================================================
+ Function: Is_number
+ Description:
+ Parameters: string input
+ ======================================================================*/
+bool Is_number( const string input )
+{
+    string::const_iterator it = input.begin();
+    
+    while( ( it != input.end( ) ) && isdigit(*it))
+    {
+        it++;
+    }
+    
+    return ( ! input.empty() && ( it == input.end( ) ) );
 }
 
 /*=====================================================================
@@ -97,7 +172,7 @@ int Rand_num_generator ( int chosen_songs[], int seed, int max )
         {
             //If the index exists it sets the boolean value to repeat array
             //then breaks out of the for loop.
-            if( chosen_questions[cntr] == rand_int )
+            if( chosen_songs[cntr] == rand_int )
             {
                 unique_item = false;
                 break;
@@ -121,20 +196,67 @@ int Rand_num_generator ( int chosen_songs[], int seed, int max )
 string Read_lines ( ifstream &input_songs )
 {
     //Variable declarations
-    string output = "";
+    string output = "",
+           temp;
     
     //Reads lines from input file until it finds a non-blank line.
     //After the line has been determined to not be blank returns that line
-    while ( getline( input_file, output ) && ( !input_file.eof( ) ) )
+    while ( getline( input_songs, temp ) && ( !input_songs.eof( ) ) )
     {
-        if ( strlen(output.c_str( ) ) == 1 || strlen( output.c_str( ) ) == 0 )
+        output = temp;
+        if ( ( strlen(temp.c_str( ) ) == 1 || strlen( temp.c_str( ) ) == 0 ) &&
+             ( ! Is_number(temp) ) )
             continue;
         
         else
             break;
     }
     
+    
+    if ( ( strlen(temp.c_str( ) ) == 1 || strlen( temp.c_str( ) ) == 0 ) &&
+        ( ! Is_number(temp) ) )
+        output = " ";
+    
     return output;
 }
 
-Song * Read_Songs
+
+/*=====================================================================
+ Function: Songs_array_mod
+ Description:
+ Parameters: ifstream input_songs
+ ======================================================================*/
+Song * Songs_array_mod ( ifstream &input_songs )
+{
+    //Varaibles declaration
+    Song * new_array;
+    string input;
+    
+    int size = Count_songs(input_songs);
+    
+    new_array = new Song[size];
+    
+    //reset file to the top of the stream
+    input_songs.clear();
+    
+    input_songs.seekg(0, ios::beg );
+    
+    //Reads all songs into the array
+    for( int i = 0; i < size; i++ )
+    {
+        new_array[i].title = Read_lines( input_songs );
+                
+        new_array[i].artist = Read_lines( input_songs );
+        
+        new_array[i].size = stod( Read_lines( input_songs ).c_str() );
+    }
+    
+    for( int i = 0; i < size; i++)
+    {
+        cout << new_array[i].title << endl
+             << new_array[i].artist << endl
+             << new_array[i].size << endl;
+    }
+    
+    return new_array;
+}
