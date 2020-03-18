330bec3887d06511544a085039e22aae757cf302
FieldMarshallObvious <noahdelangel@gmail.com>
2020-03-18 15:17:04 -0500

    Created node project and succesfully served it using index.js

---
+++ Personal Projects/Web Development/ServerSideJavascript/index.js
@@ -0,0 +1,9 @@
+//import express library
+const express = require('express');
+const app = express();
+
+//Tell the web application to listen for request at port 3000
+app.listen(3000, () => console.log('listening at 3000'));
+
+//Use express to serve static items
+app.use(express.static('public'));
