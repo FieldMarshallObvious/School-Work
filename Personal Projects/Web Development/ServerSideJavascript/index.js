1ecf53ef3324f6481d56fd69e9892641534a94cf
FieldMarshallObvious <noahdelangel@gmail.com>
2020-03-19 15:15:56 -0500

    More style guide changes, to the servside JS index.html as well as the JS practice index.html

--- Personal Projects/Web Development/ServerSideJavascript/index.js
+++ Personal Projects/Web Development/ServerSideJavascript/index.js
@@ -1,9 +1,15 @@
 //import express library
-const express = require('express');
-const app = express();
+const express = require( 'express' );
+const app = express( );
 
 //Tell the web application to listen for request at port 3000
-app.listen(3000, () => console.log('listening at 3000'));
+app.listen( 3000, ( ) => console.log( 'listening at 3000' ) );
 
 //Use express to serve static items
-app.use(express.static('public'));
+app.use( express.static( 'public' ) );
+
+
+app.post( '/api', ( request, response ) =>
+	{
+		console.log( reuqest );
+	} );
