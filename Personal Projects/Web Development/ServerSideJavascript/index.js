2fe9f779feab12b4e98c0d7f373c4b5ac1b6451c
FieldMarshallObvious <noahdelangel@gmail.com>
2020-03-19 15:45:44 -0500

    Configured index.js to recieve data from the client as JSON, at a limit of 1mb. Also configured the index.js to log that data to the console, then send it back to the client. Added logic to the client to send and recieve JSON data. Logic was also added to post data sent back from the server the console. Set the dom elements to a percision of decimal points. Some style guide changes where made as well.

--- Personal Projects/Web Development/ServerSideJavascript/index.js
+++ Personal Projects/Web Development/ServerSideJavascript/index.js
@@ -8,8 +8,20 @@ app.listen( 3000, ( ) => console.log( 'listening at 3000' ) );
 //Use express to serve static items
 app.use( express.static( 'public' ) );
 
+//Allow server to pasre incoming data as json
+app.use( express.json( {limit: '1mb'} ) );
 
 app.post( '/api', ( request, response ) =>
 	{
-		console.log( reuqest );
+		console.log( request.body );
+
+		//Sends data back to the client
+		response.json({
+			//Inform the client data was recieved
+			status: 'success',
+
+			//Send back data that was recieved
+			latitude: request.body.lat,
+			longitude: request.body.long
+		});
 	} );
