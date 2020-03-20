4058c6661ce2b21665ab60d8f9f325d808c85361
FieldMarshallObvious <noahdelangel@gmail.com>
2020-03-20 01:02:26 -0500

    Added nedb database to the node dependencies. Also added logic to log geolocation from the cleint to the database, along with a timestamp. Added logic for the website to request location only on button press. Also made marker invsible until location has been determined

--- Personal Projects/Web Development/ServerSideJavascript/index.js
+++ Personal Projects/Web Development/ServerSideJavascript/index.js
@@ -1,5 +1,9 @@
 //import express library
 const express = require( 'express' );
+
+//import NEDB database
+const Datastore = require( 'nedb' );
+
 const app = express( );
 
 //Tell the web application to listen for request at port 3000
@@ -11,9 +15,23 @@ app.use( express.static( 'public' ) );
 //Allow server to pasre incoming data as json
 app.use( express.json( {limit: '1mb'} ) );
 
+
+//Create new data base
+const database = new Datastore('database.db');
+
+//load old database
+database.loadDatabase();
+
+
 app.post( '/api', ( request, response ) =>
 	{
+		const data = request.body;
+		const timestamp = Date.now();
+		data.timestamp = timestamp;
+
 		console.log( request.body );
+
+		database.insert( data );
 
 		//Sends data back to the client
 		response.json({
@@ -22,6 +40,9 @@ app.post( '/api', ( request, response ) =>
 
 			//Send back data that was recieved
 			latitude: request.body.lat,
-			longitude: request.body.long
+			longitude: request.body.long,
+
+			//Send back timestamp
+			timestamp: timestamp
 		});
 	} );
