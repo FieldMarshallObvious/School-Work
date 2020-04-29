78ae064e7f53eed29a7cd60a5f5c0b3fe4be938e
FieldMarshallObvious <noahdelangel@gmail.com>
2020-04-29 17:07:53 -0500

    made changes to test file and que file, as weell as make file. Currently the dequeue is not working, but cannot be tested as something is not being compileed correctly

---
+++ Class Work/CS 2308/prog 5/DynQue.h
@@ -0,0 +1,124 @@
+//
+//  DynQue.h
+//  
+//
+//  Created by Noah del Angel on 4/28/20.
+//
+
+#ifndef DynQue_h
+#define DynQue_h
+#include<iostream>
+
+using namespace std;
+
+template<class T>
+class Dynque
+{
+    private:
+        struct QueueNode
+        {
+            T value;
+            QueueNode *next;
+
+        };
+    
+    QueueNode *front;
+    QueueNode *rear;
+    int numItems;
+    
+    public:
+    Dynque();
+    ~Dynque();
+    void enqueue(T);
+    void dequeue(T &);
+    bool isEmpty();
+    
+};
+
+template<class T>
+Dynque<T>::Dynque( )
+{
+
+    front = NULL;
+    rear = NULL;
+
+    numItems = 0;
+}
+
+template<class T>
+Dynque<T>::~Dynque()
+{
+    //Variable declarations
+    int value;
+    
+    //While their are items in the que
+    while( !( isEmpty( ) ) )
+    {
+        dequeue(value);
+    }
+}
+
+template<class T>
+void Dynque<T>::enqueue( T num )
+{
+
+    //Variable declarations
+    QueueNode *newNode = new QueueNode;
+    
+    
+    newNode->value = num;
+    newNode->next = NULL;
+    
+    if ( isEmpty() )
+    {
+        front = newNode;
+        rear = newNode;
+    }
+    else
+    {
+        rear->next = newNode;
+        rear = newNode;
+    }
+    
+}
+
+template<class T>
+void Dynque<T>::dequeue( T & num )
+{
+    cout << "dequeue is called "<< endl;
+    //Variable delcarations
+    QueueNode *temp;
+    
+    if( isEmpty() )
+    {
+        cout << "The queue is empty. " << endl;
+        return;
+    }
+    
+    else
+    {
+        
+        num = front->value;
+        temp = front;
+        
+        front= front->next;
+        
+        delete temp;
+        
+        numItems--;
+        cout << num << endl;
+    }
+}
+
+template<class T>
+bool Dynque<T>::isEmpty()
+{
+    bool output = true;
+    
+    if ( numItems > 0 )
+        output = false;
+    
+    return output;
+}
+
+#endif /* DynQue_h */
--- Class Work/CS 2308/prog 5/test.cpp
+++ Class Work/CS 2308/prog 5/test.cpp
@@ -8,23 +8,46 @@
 #include <iostream>
 #include <cstdlib>
 #include "DynStack.h"
+#include "DynQue.h"
 
 using namespace std;
 
 int main ( )
 {
     
+    //Test stack push and pop
     DynStack<int> stack;
     int poppedval = -2;
+    cout << "Pushing to a stack" << endl;
     stack.push(1);
     stack.push(2);
     stack.push(3);
     stack.push(4);
     stack.push(5);
-    while( poppedval != -1 )
+    
+    stack.pop(poppedval);
+    for( ;poppedval != -1;  stack.pop(poppedval) )
     {
-        stack.pop(poppedval);
         cout << "The popped value is : " << poppedval << endl;
+    }
+    
+    //Test Queue enqeue and dequeue
+    Dynque<int> que;
+    poppedval = -2;
+    int cntr = 0;
+    cout << "Pushing to a que" << endl;
+    que.enqueue(1);
+    que.enqueue(2);
+    que.enqueue(2);
+    que.enqueue(4);
+    que.enqueue(5);
+    
+    for ( ; poppedval != -1; que.dequeue(poppedval) )
+    {
+        cntr ++;
+        cout << "The dequeued value is s" << poppedval << endl;
+        if( cntr >= 5 )
+            break;
     }
     return 0;
 }
