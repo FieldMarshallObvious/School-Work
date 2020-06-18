f7100e8fcaec65fe6f34754bf48ab0dc656f1ba5
FieldMarshallObvious <noahdelangel@gmail.com>
2020-06-18 15:49:12 -0500

    Added code to insert API data into the database. As well as function to find data in the database. Added a function to clear database upon start of server. Also added code to return data from the findInfo function once it has recieved data

--- Personal Projects/Web Development/SP500 Website/index.js
+++ Personal Projects/Web Development/SP500 Website/index.js
@@ -52,17 +52,80 @@ app.use((req, res, next) => {
 //Returns API data from database
 app.get( '/index_key_ratios/:equity', aync(request, response) => 
 {
-	
+	const index = request.params.index;
+	findInfo(database, index).then((results) => 
+	{
+		console.log(index, ": ", results.shiller_pe);
+		response.json([results.pe], [results.shiller_pe], [results.dividend_yield]);
+	});
 });
-
 //Callback function for database search
 function readDatabase(err)
 {
 	console.log(err);
 }
 
+//Finds the index of desired information based on ID's
+//Then returns the information in that index
+function findInfo( database, indexInput)
+{
+	var output;
+	database.find({}, (err, data) => {
+		if (err)
+		{
+			response.end();
+			return;
+		}
+
+		for( var i = 0; i < data.length; i++ )
+		{
+			if(data[i].index == indexInput)
+			{
+				output = data[i];
+				break;
+			}
+		}
+	});
+
+	return new Promise( resolve => 
+	{
+		setTimeout(() => {
+			if( output != undefined )
+				resolve(output);
+			else
+				reject("Recovery failed");
+
+		}, 2000, output);
+	});
+}
+
+//Function clears database
+function clearDB()
+{
+	//Clear database
+	db.remove({}, { multi: true }, function (err, numRemoved) {});
+}
+
 //Updates the data inside the database
 async function updateDataInDatabase()
 {
+	//Get current date
+	var date = new Date,
+		year = date.getFullYear(),
+		month = (date.getMonth() + 1),
+		day = date.getDate();
 
+	var curdate = [year, month, day].join('-');
+
+	var newest_available_date;
+
+	//Clear data from last update
+	database.remove({}, { multi: true }, function (err, numRemoved){});
+
+
+	//Get SP500 data
+	output = await API_interface.returnSP500( curdate, quandlApiKey, serverfetch );
+
+	//Send all acquired data to the database
+	database.insert( output );
 }
