77dbe7b58e000c36adc29e5160b257aaf771691d
FieldMarshallObvious <noahdelangel@gmail.com>
2020-06-17 21:01:37 -0500

    Added some more skeleton code, and installed nedb library

--- Personal Projects/Web Development/SP500 Website/index.js
+++ Personal Projects/Web Development/SP500 Website/index.js
@@ -1,6 +1,12 @@
 //import express library
 const express = require ( 'express' );
 
+//Import dotenv into app
+require('dotenv').config();
+
+//Import NEDB database
+const Datastore = require( `nedb` );
+
 
 //Setup library to var app
 const app = express( );
@@ -19,3 +25,44 @@ process.on('SIGINT', function() {
   	
   	process.exit(1);
 });
+
+//Setup database
+const database = new Datastore('database.db');
+
+//Load old database
+database.loadDatabase();
+
+//Get quandlAPIkey from enviroment
+const quandlApiKey = process.env.API_KEY_QUANDL;
+
+//Update the database
+updateDataInDatabase();
+
+//Resolve CORS issues
+app.use((req, res, next) => {
+  	res.setHeader("Access-Control-Allow-Origin", "*");
+  	res.setHeader(
+    	"Access-Control-Allow-Methods",
+    	"OPTIONS, GET, POST, PUT, PATCH, DELETE" // what matters here is that OPTIONS is present
+  	);
+  	res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  	next();
+});
+
+//Returns API data from database
+app.get( '/index_key_ratios/:equity', aync(request, response) => 
+{
+	
+});
+
+//Callback function for database search
+function readDatabase(err)
+{
+	console.log(err);
+}
+
+//Updates the data inside the database
+async function updateDataInDatabase()
+{
+
+}
