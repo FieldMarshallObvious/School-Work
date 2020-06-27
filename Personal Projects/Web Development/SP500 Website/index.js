b72f5e568ab15d7465c374fb19a2915bdfd00134
FieldMarshallObvious <noahdelangel@gmail.com>
2020-06-27 02:02:46 -0500

    removed time check for testing purposes

--- Personal Projects/Web Development/SP500 Website/index.js
+++ Personal Projects/Web Development/SP500 Website/index.js
@@ -277,7 +277,21 @@ async function updateEquityPrice()
 	}
 	else
 	{
-		console.log('Market is closed for the day');
-		console.log('NOT CHECKING CURRENT PRICE');
+		//console.log('Market is closed for the day');
+		//console.log('NOT CHECKING CURRENT PRICE');
+				API_interface.returnPrice( bloombergAPIkey, serverfetch, unirest ).then((results) => 
+		{
+			console.log("Current Price of the index: ", results);
+			
+			//Get timestamp
+			timestamp = getTimestamp();
+
+			output = {
+				price: results,
+				index: timestamp
+			};
+
+			database.insert( output );
+		});
 	}
 }
