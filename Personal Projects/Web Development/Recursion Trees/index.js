9fecd5d316d53880b721c64f729cbc0898d040d5
FieldMarshallObvious <noahdelangel@gmail.com>
2020-07-18 15:17:23 -0500

    Changed the way that random curves are generated. Branch random curves are generated indpenedtly of main trunk curves. The trunk curves are also generated at the press of the button, and not upon each iteration.

--- Personal Projects/Web Development/Recursion Trees/index.js
+++ Personal Projects/Web Development/Recursion Trees/index.js
@@ -6,16 +6,24 @@ canvas.height = window.innerHeight;
 
 const ctx = canvas.getContext('2d');
 
+let curve = ( Math.random() * 10 ) + 10, curve1 = ( Math.random() * 20 ) + 2 , curve2 = ( Math.random() * 50 ) + 1;
+
+drawTree( canvas.width/2, canvas.height-80, 120, 0, 25, 'brown', 'pink');
+
+generateButton.addEventListener('click', generateRandomTree);
+
 function drawTree(startX, startY, len, angle, branchWidth, mainColor, leavesColor )
 {
 	//Create canvas path
 	ctx.beginPath();
 	ctx.save();
 
-	//Create stroke colors
+	//Create stroke colors and shadow
 	ctx.strokeStyle = mainColor;
 	ctx.fillStyle = leavesColor;
 	ctx.lineWidth = branchWidth;
+	ctx.shadowBlur = 15;
+	ctx.shadowColor = 'rgba(0, 0, 0, 0.5)';
 
 	//Place canvas on certian x and y posistion
 	//and rotate canvas to the passed angle
@@ -26,24 +34,86 @@ function drawTree(startX, startY, len, angle, branchWidth, mainColor, leavesColo
 	ctx.moveTo(0,0);
 
 	//Create a line to the length
-	ctx.lineTo(0, -len);
+	//ctx.lineTo(0, -len);
+
+	//Curve the tree based on angle
+	if( angle > 0 )
+	{
+		ctx.bezierCurveTo( curve1, -len/2, 10, -len/2, 0, -len );
+	}
+	else
+	{
+		ctx.bezierCurveTo( curve2, -len/2, -10, -len/2, 0, -len );
+	}
 
 	//Draw the stroke
 	ctx.stroke();
 
 	//If the length grows past 10, end recursion
-	if(len < 10)
+	//and draw leaves
+	if(len < 15)
 	{
-		//Pops last entry in drawing stack
+		//Draw leafs
+		ctx.beginPath();
+		ctx.arc(0, -len, 10, 0, Math.PI/2);
+		ctx.fill();
+
 		ctx.restore();
+
+
 		return;
 	}
 
+	//Randomize the curve
+	//NOTE uncomment this if you want to make the
+	//tree's less symmetrical
+	//curve = ( Math.random() * 10 ) + 10;
+
 	//Implements recursion, to create fork branches
-	drawTree(0, -len, len * 0.75, angle + 5, branchWidth, mainColor, leavesColor);
-	drawTree(0, -len, len * 0.75, angle - 5, branchWidth, mainColor, leavesColor);
+	drawTree(0, -len, len * 0.75, angle + curve, branchWidth * 0.6 );
+	drawTree(0, -len, len * 0.75, angle - curve, branchWidth * 0.6 );
 
 	ctx.restore();
 }
 
-drawTree( canvas.width/2, canvas.height-80, 120, 0, 2, 'brown', 'green');
+//This function is called when the random tree button is pressed 
+function generateRandomTree() {
+	ctx.clearRect(0,0,canvas.width, canvas.height);
+
+	//Generate values required to pass to the tree function
+	let centerPointX = canvas.width/2;
+	let startingPointY = canvas.height - 80;
+	let len = Math.floor( (Math.random() * 20) + 100 );
+	let angle = 0;
+	let branchWidth = (Math.random() * 140) + 1;
+	let mainColor = 'rgb(' + Math.random() * 255 + ',' + Math.random() * 255 + ',' + Math.random() * 255 + ')';
+	let leavesColor = preventDuplicates(mainColor);
+
+
+	//Change color of the button to match
+	generateButton.style.background = mainColor;
+
+	//Randomize curve of branches
+	curve = ( Math.random() * 10 ) + 10
+	curve1 = ( Math.random() * 20 ) + 2;
+	curve2 = ( Math.random() * 50 ) + 1;
+
+	drawTree(centerPointX, startingPointY, len, angle, branchWidth, mainColor, leavesColor);
+}
+
+//This function prevents the branch color
+//from being the same as the leaf color
+function preventDuplicates( previous )
+{
+	//Variable declarations
+	var output = 'rgb(' + Math.random() * 255 + ',' + Math.random() * 255 + ',' + Math.random() * 255 + ')';
+
+	//While the current color equals this color, try to find a new color
+	while( output == previous )
+	{
+		output = 'rgb(' + Math.random() * 255 + ',' + Math.random() * 255 + ',' + Math.random() * 255 + ')';
+	}
+
+	//Return once a new color has been found
+	return output;
+}
