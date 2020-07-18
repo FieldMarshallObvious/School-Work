a5e5e0a543fa9caf9a7598f7eb8995dcaf1b9036
FieldMarshallObvious <noahdelangel@gmail.com>
2020-07-18 14:17:01 -0500

    Fixed recursion call, now tree draws correctly

--- Personal Projects/Web Development/Recursion Trees/index.js
+++ Personal Projects/Web Development/Recursion Trees/index.js
@@ -4,34 +4,46 @@ const generateButton = document.querySelector('.generate-tree-button');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-const ctx = canvas.getContext("2d");
+const ctx = canvas.getContext('2d');
 
 function drawTree(startX, startY, len, angle, branchWidth, mainColor, leavesColor )
 {
-	//Canvas items to create tree, and branches
+	//Create canvas path
 	ctx.beginPath();
 	ctx.save();
+
+	//Create stroke colors
 	ctx.strokeStyle = mainColor;
 	ctx.fillStyle = leavesColor;
 	ctx.lineWidth = branchWidth;
+
+	//Place canvas on certian x and y posistion
+	//and rotate canvas to the passed angle
 	ctx.translate(startX, startY);
 	ctx.rotate(angle * Math.PI/180);
+
+	//Move the tree to the center
 	ctx.moveTo(0,0);
+
+	//Create a line to the length
 	ctx.lineTo(0, -len);
+
+	//Draw the stroke
 	ctx.stroke();
 
 	//If the length grows past 10, end recursion
 	if(len < 10)
 	{
+		//Pops last entry in drawing stack
 		ctx.restore();
 		return;
 	}
 
 	//Implements recursion, to create fork branches
-	drawTree(0, -len * 0.75, angle + 5, branchWidth);
-	drawTree(0, -len * 0.75, angle - 5, branchWidth);
+	drawTree(0, -len, len * 0.75, angle + 5, branchWidth, mainColor, leavesColor);
+	drawTree(0, -len, len * 0.75, angle - 5, branchWidth, mainColor, leavesColor);
 
 	ctx.restore();
 }
 
-drawTree( canvas.width/2, canvas.height-80, 120, 0, 2, "brown", "green");
+drawTree( canvas.width/2, canvas.height-80, 120, 0, 2, 'brown', 'green');
