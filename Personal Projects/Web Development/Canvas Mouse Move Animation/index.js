f216db105606336edc8d5afb451d11b38c7c7597
FieldMarshallObvious <noahdelangel@gmail.com>
2020-07-25 16:59:34 -0500

    Slowed animation down, and added condition for window resize

--- Personal Projects/Web Development/Canvas Mouse Move Animation/index.js
+++ Personal Projects/Web Development/Canvas Mouse Move Animation/index.js
@@ -7,16 +7,18 @@ canvas.height = window.innerHeight;
 ctx.textBaseline = 'middle';
 
 //Variable declarations
-let lettersArray = ['L', 'A', 'B', 'O', 'R', 'A', 'T', 'O', 'R', 'Y'];
+let lettersArray = ['C', 'O', 'O', 'L'];
 let hue = 0;
 let particles = [];
-let numberofParticles = (canvas.width * canvas.height ) / 7000;
+let numberofParticles = (canvas.width * canvas.height ) / 5000;
+
+console.log("number of particles", numberofParticles);
 
 const mouse = {
 	x: 0,
 	y: 0,
 	radius: 20,
-	autopilotAngle: 0
+	autopilotAngle: 0,
 }
 
 //Every time the mouse moves
@@ -73,15 +75,14 @@ class Particle
 	{
 		if( mouse.x == undefined && mouse.y == undefined )
 		{
-			console.log(mouse.autopilotAngle);
-			let newX = mouse.radius * canvas.width/200 * Math.sin(mouse.autopilotAngle * (Math.PI/180));
+			let newX = mouse.radius * canvas.width/50 * Math.cos(mouse.autopilotAngle * (Math.PI/180));
 
-			let newY = mouse.radius * canvas.height/200 * Math.cos(mouse.autopilotAngle * (Math.PI/180));
+			let newY = mouse.radius * canvas.height/50 * Math.sin(mouse.autopilotAngle * (Math.PI/90));
 
 			mouse.x = newX + canvas.width/2;
 			mouse.y = newY + canvas.height/2;
 		}
-		mouse.autopilotAngle += 0.07;
+		mouse.autopilotAngle += 0.01;
 	}
 }
 
@@ -93,22 +94,25 @@ class Particle
 function handleOverlap()
 {
 	//Variable declarations
-	let overlapping = false,
-		protection = 500,
-		counter = 0;
+	let overlapping = false;
+	let protection = 500;
+	let counter = 0;
 
 	//Create random posistion around mouse for particle
 	while( particles.length < numberofParticles && counter < protection )
 	{
 		let randomAngle = Math.random() * 2 * Math.PI;
 
-		let randomRadius = mouse.radius * Math.sqrt(Math.random());
+		let randomRadius = mouse.radius * Math.random();
 
 		let particle = {
 			x: mouse.x + randomRadius + Math.cos(randomAngle),
 			y: mouse.y + randomRadius + Math.sin(randomAngle),
-			radius: Math.floor(Math.random() * 30) + 10
+			radius: Math.floor(Math.random() * 30) + 10,
 		};
+
+		overlapping = false;
+
 		//Check for particles that collide
 		for( let i = 0; i < particles.length; i++ )
 		{
@@ -118,7 +122,7 @@ function handleOverlap()
 			let dy = particle.y - previousParticle.y;
 			let distance = Math.sqrt( dx * dx + dy * dy);
 
-			if( distance < particle.radius + previousParticle.radius )
+			if( distance < (particle.radius + previousParticle.radius) )
 			{
 				overlapping = true;
 				break;
@@ -128,7 +132,6 @@ function handleOverlap()
 
 		//If there is no overlap, add the paritcle
 		//to the array
-
 		if( !overlapping )
 		{
 			particles.unshift(new Particle(particle.x, particle.y, particle.radius));
@@ -141,7 +144,7 @@ function handleOverlap()
 
 //This function creates animation
 //loop
-function animate( )
+function animate(  )
 {
 	ctx.clearRect( 0, 0, canvas.width, canvas.height );
 	
@@ -152,42 +155,48 @@ function animate( )
 		particles[i].autoMove();
 	}
 
+	handleOverlap();
 	//Remove old particles
 	if ( particles.length >= numberofParticles )
 	{
-		for( let i = 0; i < 5; i++ )
+		for( let i = 0; i < 3; i++ )
 		{
 			particles.pop();
 		}
 	}
 
-	handleOverlap();
-
 	//Increment hue
-	hue+=5.5;
-
+	hue+=2;
+ 
 	//Implement recursion
 	requestAnimationFrame(animate);
 }
 
-
-handleOverlap();
-
 animate();
 
-let autopilot = setInterval(function() {
-	mouse.x = undefined;
-	mouse.y = undefined;
-}, 40);
-
-canvas.addEventListener('mouseleave', function() {
-	autopilot  = setInterval(function() {
-	mouse.x = undefined;
-	mouse.y = undefined;
-}, 40);
+window.addEventListener("resize", function(){
+    particles = [];
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    numberofParticles =  (canvas.width * canvas.height ) / 5000;
+    ctx.textBaseline = "middle";
+    radius = window.innerWidth/5;
+    handleOverlap();
 });
 
-cavnas.addEventListener('mouseenter', function(){
-	clearInterval(autopilot);
-	autopilot = undefined;
+let autopilot = setInterval(function(){
+        mouse.x = undefined;
+        mouse.y = undefined;
+}, 4);
+
+canvas.addEventListener("mouseleave", function(){
+    autopilot = setInterval(function(){
+        mouse.x = undefined;
+        mouse.y = undefined;
+    }, 4);
+});
+
+canvas.addEventListener("mouseenter", function(){
+    clearInterval(autopilot);
+    autopilot = undefined;
 });
