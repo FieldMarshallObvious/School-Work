8547af6acf294487f2783b4de3759f80d0dbf6df
FieldMarshallObvious <noahdelangel@gmail.com>
2020-08-22 12:36:22 -0500

    Changed commebts to be more accurate

--- Personal Projects/Assembler/Getting Started/ex3.asm
+++ Personal Projects/Assembler/Getting Started/ex3.asm
@@ -3,14 +3,14 @@ global start
 section .text
 
 start:
-    mov rax, 0x2000001 ; exit status is 4
-    mov rdi, 1 ; sys_exit system call
+    mov rax, 0x2000001 ; exit status system call
+    mov rdi, 1 ; sys_exit system call set to 1
 
 
     jmp skip ; jump to "skip label"
 
-    mov rax, 0x2000001 ; exit status is 1
-    mov rdi, 0 ; sys_exit system call
+    mov rax, 0x2000001 ; exit status system call
+    mov rdi, 0 ; sys_exit system call set to 0
     syscall ; Invoke the kernel
 
 
