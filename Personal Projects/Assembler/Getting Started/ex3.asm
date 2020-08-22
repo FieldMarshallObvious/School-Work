a6eb02ecc74e845d7e7e97a90687ef17eac5cf5d
FieldMarshallObvious <noahdelangel@gmail.com>
2020-08-21 19:08:52 -0500

    Tried making jmp command work, still seems to fail to work for some reason

---
+++ Personal Projects/Assembler/Getting Started/ex3.asm
@@ -0,0 +1,19 @@
+global start
+
+section .text
+
+start:
+    mov rax, 0x2000004 ; exit status is 4
+    mov rdi, 1 ; sys_exit system call
+
+
+    jmp skip ; jump to "skip label"
+
+    mov rax, 0x2000001 ; exit status is 1
+    mov rdi, 0 ; sys_exit system call
+    syscall ; Invoke the kernel
+
+
+skip:
+    syscall ; Invoke the kernel
+
