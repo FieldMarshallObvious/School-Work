cad9d9ee87c5b2d00f757e8076725199ad42175b
FieldMarshallObvious <noahdelangel@gmail.com>
2020-08-25 16:07:27 -0500

    Reorginaized folders to be cleaner, and renamed parent folder from Assembler to Assembly to make more sense. Also attempted to fix console commands assembly code. Now getting a segementation fault error

--- Personal Projects/Assembly/Getting Started/Console Commands/consolecommands.asm
+++ Personal Projects/Assembly/Getting Started/Console Commands/consolecommands.asm
@@ -1,16 +1,14 @@
-global _start
-extern puts
+global start
+extern _puts
 section .text
 
-_start: 
+start: 
     push rdi ; save registers that puts uses
     push rsi ; align stack before call
     sub rsp, 8 ; align stack before call
 
     mov rdi, [rsi] ; the argument string to display
-    push rdi;
-    call puts ; prints string
-    pop rdi;
+    call _puts ; prints string
 
     add rsp, 8 ; restore %rso ti pre-aligned value
     pop rsi ; resotre registers puts used
@@ -18,6 +16,7 @@ _start:
 
     add rsi, 8 ; point to next argument
     dec rdi ; count down
-    jnz _start ; if not done counting keep going
+    jnz start ; if not done counting keep going
 
-    ret
+
+    syscall
