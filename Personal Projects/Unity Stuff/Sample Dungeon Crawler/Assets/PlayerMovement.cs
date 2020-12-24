6f87c70cb9623aa356ee6ea783ab6f3d0b3fc616
FieldMarshallObvious <noahdelangel@gmail.com>
2020-12-23 22:07:24 -0600

    Added cpde for unity project that I'm working on. Trying to implement new method for sprite animation

---
+++ Personal Projects/Unity Stuff/Sample Dungeon Crawler/Assets/PlayerMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private float speed;
+    private SpriteAnimation spriteAnimation;
+    public Sprite[] spriteArray;
+    private SpriteRenderer SpriteRenderer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        speed = 1;
+        spriteAnimation = new SpriteAnimation(spriteArray, SpriteRenderer);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        GetComponent<Rigidbody2D>().velocity = new Vector2(horizontal * speed, vertical * speed);
+
+        Debug.Log("Entering function");
+        spriteAnimation.UpdateSprite(false);
+    }
+}
---
+++ Personal Projects/Unity Stuff/Sample Dungeon Crawler/Assets/SpriteAnimation.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteAnimation : MonoBehaviour
+{
+    private SpriteRenderer SpriteRenderer;
+    private Sprite[] spriteArray;
+    private Sprite curSprite;
+    private int curIndex = 0;
+    private float changeTime = 0.05f;
+    private bool passed = false;
+    private bool reverse = false;
+
+
+    // Start is called before the first frame update
+    public SpriteAnimation( Sprite[] inputArray, SpriteRenderer inputSpriteRender )
+    {
+        SpriteRenderer = inputSpriteRender;
+        Debug.Log("Created function");
+        this.GetComponent<SpriteRenderer>().sprite = curSprite;
+        StartCoroutine("ChangeSprite");
+    }
+
+    // Update is called once per frame
+    public void UpdateSprite( bool reverse )
+    {
+        Debug.Log("In function");
+        this.GetComponent<SpriteRenderer>().sprite = SpriteRenderer.sprite;
+    }
+
+    IEnumerator ChangeSprite( )
+    {
+
+        SpriteRenderer.sprite = spriteArray[curIndex];
+        if (reverse == true)
+            SpriteRenderer.flipX = true; 
+        else
+            SpriteRenderer.flipX = false;
+
+        if (passed != true)
+            curIndex++;
+        else
+            curIndex--;
+        if (curIndex >= spriteArray.Length - 1)
+        {
+            passed = true;
+        }
+        if (curIndex <= 0 )
+        {
+            passed = false;
+        }
+
+        yield return new WaitForSeconds(changeTime);
+        StartCoroutine("ChangeSprite");
+    }
+
+}
