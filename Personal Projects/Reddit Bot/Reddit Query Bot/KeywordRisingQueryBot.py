90460c56761808d7e8d1d554cdfc3206918b7fc5
unknown <noahdelangel@gmail.com>
2021-01-27 00:13:23 -0600

    Got a list of the tickers from finhub instead, and reworked logic to go through tickers. The code is now producing an index out of range error

--- Personal Projects/Reddit Bot/Reddit Query Bot/KeywordRisingQueryBot.py
+++ Personal Projects/Reddit Bot/Reddit Query Bot/KeywordRisingQueryBot.py
@@ -1,32 +1,50 @@
 import praw
-from get_all_tickers import get_tickers as gt
+import requests
+import json
 
 # Get reddit bot and subreddit
 reddit = praw.Reddit('bot2')
 
 subreddit = reddit.subreddit("wallstreetbets")
 
-# Get all stock tickers from Yahoo Finance
-lOfTickers = gt.get_tickers()
+# Get all stock tickers from FinHub
+r = requests.get('https://finnhub.io/api/v1/stock/symbol?exchange=US&token=c06u89f48v6rt4njqhr0')
+rJson = json.loads(r.text)
+lOfTickers = [""]
 numofTicks = []
 
+
+
+
+# Search through all the submissions in the rising tab
 for submission in subreddit.rising(limit=24):
     Title = str(submission.title)
     word = ""
     for char in Title:
         index = 0
+
+        # Build words out of the submission string
         if(char != " " and char != "," and char != "." and char != "!" and char != "?"):
             word = word + char
-            print(word)
-        if(char == " " or char == "," or char == "." or char == "!" or char == "?" ):
-            print("CHECKING WORD")
-            for ticker in lOfTickers:
-                index = index + 1
-                if(word == lOfTickers):
-                    print(word)
-                    if(numofTicks[index] == None):
-                        numofTicks.append(1)
-                    else:
-                        numofTicks[index] = numofTicks[index] + 1
 
+        # If the char is not part of the word check it against tickers
+        if(char == " " or char == "," or char == "." or char == "!" or char == "?" ):
+
+            for ticker in rJson:
+                if(word == ticker["displaySymbol"]):
+                    found = False
+
+                    # Find the equivalent tickers in the array 
+                    for foundTicker in lOfTickers:
+                        index = index + 1
+                        if(foundTicker == word):
+                            numofTicks[index] = numofTicks[index] + 1
+                            found = True
+
+                    # If the ticker has not been found yet append it
+                    if(found == False):
+                        lOfTickers[index].append(word)
+                        numofTicks[index].append(1)
+
+            # Reset word
             word = ""
