e4978b774338813ceedae887d163720aa1a113a1
unknown <noahdelangel@gmail.com>
2021-02-06 13:58:56 -0600

    Completed almost all menu operations, with safeguards in case the user enters an unkown input. Added functions to modify the seating chart, and to initialize the seating chart. Also changed all functions invovled with the seatingChart to take the array

--- Class Work/CS 3358/Prog 2/SP21_3558_252_Noah_delAngel_PG2.cpp
+++ Class Work/CS 3358/Prog 2/SP21_3558_252_Noah_delAngel_PG2.cpp
@@ -9,24 +9,28 @@
 #include <iostream>
 #include<vector>
 
-void menu( );
-std::string printMainMenu( );
-void printSeatRow( );
-void seatingChart( );
+void menu( char[][13] );
+void printSeatRow( char[][13] );
+void modifySeatingChart( char, char[][13] );
+void initSeatingChart( char[][13] );
 
 using namespace std;
 
 int main ( )
 {
-    menu();
+    char seatingChar[6][13];
+    menu(seatingChar);
     return 0;
 }
 
-void menu( )
+void menu( char seatingChart[][13] )
 {
     // Variable declarations
-    char inputClass;
-    bool endApp = false;
+    char inputClass,
+         assignAnotherSeat;
+    bool endApp = false,
+         endQ = false,
+         passed = false;
 
     while( endApp == false )
     {
@@ -37,24 +41,142 @@ void menu( )
         cout << "     3. Enter B for Business Class" << endl;
         cout << "     4. Enter E for Economy class" << endl;
         cout << "     5. Enter x to Exit the App.  ";
+
+        cin >> inputClass;
+
+        inputClass = toupper(inputClass);
+
+        cout << endl;
+
+        if( inputClass == 'F' || inputClass == 'B' || inputClass == 'E' )
+        {
+            modifySeatingChart(inputClass,seatingChart);
+            passed = true;
+        }
+
+        else if( inputClass == 'X' )
+            endApp = true;
+
+        else
+            cout << "Invalid Choice" << endl
+                 << endl;
+
+        if( passed == true )
+        {
+            endQ = false;
+            while( endQ == false )
+            {
+                cout << "Would you like to Assign another seat? (Y/N) -- ";
+
+                cin >> assignAnotherSeat;
+
+                assignAnotherSeat = toupper(assignAnotherSeat);
+
+                cout << endl;
+
+                if( assignAnotherSeat == 'Y' )
+                {
+                    endApp = false;
+                    endQ = true;
+                }
+
+                else if( assignAnotherSeat == 'N' )
+                {
+                    endApp = true;
+                    endQ = true;
+                }
+            }
+
+
+
+        }
+
+        // Reset vars
+        passed = false;
     }
 
 }
 
-string printMainMenu( )
+void modifySeatingChart( char row, char seatingChart[][13] )
 {
     // Variable declarations
-    char inputClass;
+    int index, minRow, maxRow, rowNum;
+    bool foundSeat = false;
+    char rowChar, seatloc, charIndex;
+    string possibleRows;
 
-    cout << "Enter Your Ticket Type" << endl << endl;
+    // Determine row
+    switch(row)
+    {
+        case 'F':
+            index = 0;
+            possibleRows = "( 1 - 2 )";
+            minRow = 0;
+            maxRow = 1;
+            break;
 
-    cout << "     1. Enter D for Displaying the Seating Chart" << endl;
-    cout << "     2. Enter F for First class" << endl;
-    cout << "     3. Enter B for Business Class" << endl;
-    cout << "     4. Enter E for Economy class" << endl;
-    cout << "     5. Enter x to Exit the App.  ";
+        case 'B':
+            index = 2;
+            possibleRows = "( 3 - 7 )";
+            minRow = 2;
+            maxRow = 6;
+            break;
 
-    cin >> inputClass;
+        case 'E':
+            index = 7;
+            possibleRows = "( 8 - 13 )";
+            minRow = 7;
+            maxRow = 12;
+            break;
+    }
 
-    return inputClass;
+    while( foundSeat == false )
+    {
+        cout << "Enter Row" << possibleRows << "and seat ( A - F ) " << endl;
+        cout << "Enter Row ";
+        cin >> rowChar;
+
+        cout << "Enter Seat ";
+        cin >> seatloc;
+        cout << endl;
+
+        // Convert seat location char to uppercase
+        seatloc = toupper(seatloc);
+
+        // Check if the rowChar is a digit
+        if( isdigit( rowChar ) )
+        {
+            rowNum = (int)rowChar - 48;
+
+            // Check if is in range
+            if( rowNum - 1 >= minRow && rowNum - 1 <= maxRow )
+                // Check if seat is within range
+                if( seatloc >= 'A' && seatloc <= 'F' )
+                {
+                    charIndex = 'A';
+                    index = 0;
+                    while( charIndex != seatloc )
+                        charIndex++;
+                        index++;
+                    seatingChart[rowNum][index] = 'X';
+                    foundSeat = true;
+
+                    cout << "Seat: Row " << rowChar << " Seat " << seatloc
+                         << " is assigned" << endl;
+
+                }
+
+                else
+                    cout << "Invalid Seat" << endl;
+
+            else
+                cout << "Invalid Row Number2" << endl;
+
+        }
+
+        else
+            cout << "Invalid Row Number1" << endl;
+    }
+
 }
+
