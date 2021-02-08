20bbb1f6d74c1c29fcc662034a4626328a8f1e10
unknown <noahdelangel@gmail.com>
2021-02-08 11:26:17 -0600

    Added stylizations to output, and added header and footer statements. Also added comments throughout almost all functions. Modify modifySeatingChart function logic so that it places seats in the correct place. Also added one more if statement to the menu to make D option useable

--- Class Work/CS 3358/Prog 2/SP21_3558_252_Noah_delAngel_PG2.cpp
+++ Class Work/CS 3358/Prog 2/SP21_3558_252_Noah_delAngel_PG2.cpp
@@ -9,21 +9,36 @@
 #include <iostream>
 #include<vector>
 
-void menu( char[][13] );
-void printSeatRow( char[][13] );
-void modifySeatingChart( char, char[][13] );
-void initSeatingChart( char[][13] );
+void menu( char[][7] );
+void printChart( char[][7] );
+void modifySeatingChart( char, char[][7] );
+void initSeatingChart( char[][7] );
 
 using namespace std;
 
 int main ( )
 {
-    char seatingChar[6][13];
-    menu(seatingChar);
+    char assignedSeats[13][7];
+
+    cout << "Welcome to My Airline APP." << endl << endl;
+    cout << "The purpose of this app is to assign seats for your reservation."
+         << endl;
+    cout << "This airplane has 13 rows. with six seats in each row." << endl
+         << endl;
+    cout << "     - Rows 1 and 2 are first class" << endl;
+    cout << "     - Rows 3 and 7 are business class" << endl;
+    cout << "     - Rows 8 through 13 are economy class " << endl << endl;
+
+    initSeatingChart( assignedSeats);
+    menu(assignedSeats);
+
+    cout << "Thanks for using this APP." << endl;
+    cout << "Noah del Angel" << endl;
+    cout << "2 - 15 - 2021" << endl;
     return 0;
 }
 
-void menu( char seatingChart[][13] )
+void menu( char seatingChart[][7] )
 {
     // Variable declarations
     char inputClass,
@@ -48,7 +63,15 @@ void menu( char seatingChart[][13] )
 
         cout << endl;
 
-        if( inputClass == 'F' || inputClass == 'B' || inputClass == 'E' )
+
+        // Determine the option chosen by the menu
+        if( inputClass == 'D' )
+        {
+            printChart( seatingChart );
+            passed = true;
+        }
+
+        else if( inputClass == 'F' || inputClass == 'B' || inputClass == 'E' )
         {
             modifySeatingChart(inputClass,seatingChart);
             passed = true;
@@ -61,9 +84,13 @@ void menu( char seatingChart[][13] )
             cout << "Invalid Choice" << endl
                  << endl;
 
+        // If a seat assignment option has been chosen
+        // Ask the user if they want to assign the seat again
         if( passed == true )
         {
             endQ = false;
+
+            // Continue the loop until a valid option has been enterd
             while( endQ == false )
             {
                 cout << "Would you like to Assign another seat? (Y/N) -- ";
@@ -74,16 +101,25 @@ void menu( char seatingChart[][13] )
 
                 cout << endl;
 
+                // If the user enters yes, break this loop
+                // and continue the program
                 if( assignAnotherSeat == 'Y' )
                 {
                     endApp = false;
                     endQ = true;
                 }
 
+                // If the user enters no, break this loop
+                // and end the program
                 else if( assignAnotherSeat == 'N' )
                 {
                     endApp = true;
                     endQ = true;
+                }
+
+                else
+                {
+                    cout << "Please enter a valid option." << endl << endl;
                 }
             }
 
@@ -97,7 +133,22 @@ void menu( char seatingChart[][13] )
 
 }
 
-void modifySeatingChart( char row, char seatingChart[][13] )
+void printChart( char seatingChart[][7] )
+{
+    for( int i = 0; i < 13; i++ )
+    {
+        cout << "Row " << i + 1;
+        if( i < 9 )
+            cout << "   ";
+        else
+            cout << "  ";
+        for( int j = 0; j < 7; j++ )
+            cout << seatingChart[i][j] << " ";
+        cout << endl;
+    }
+}
+
+void modifySeatingChart( char row, char seatingChart[][7] )
 {
     // Variable declarations
     int index, minRow, maxRow, rowNum;
@@ -119,7 +170,7 @@ void modifySeatingChart( char row, char seatingChart[][13] )
             index = 2;
             possibleRows = "( 3 - 7 )";
             minRow = 2;
-            maxRow = 6;
+            maxRow = 7;
             break;
 
         case 'E':
@@ -153,14 +204,20 @@ void modifySeatingChart( char row, char seatingChart[][13] )
                 // Check if seat is within range
                 if( seatloc >= 'A' && seatloc <= 'F' )
                 {
+                    // Determine the location of the seat
                     charIndex = 'A';
                     index = 0;
                     while( charIndex != seatloc )
                         charIndex++;
                         index++;
-                    seatingChart[rowNum][index] = 'X';
+
+                    // Assign the position in the seating chart to X
+                    seatingChart[rowNum-1][index-1] = 'X';
+
+                    // End loop
                     foundSeat = true;
 
+                    // Display assigned seat
                     cout << "Seat: Row " << rowChar << " Seat " << seatloc
                          << " is assigned" << endl;
 
@@ -170,7 +227,7 @@ void modifySeatingChart( char row, char seatingChart[][13] )
                     cout << "Invalid Seat" << endl;
 
             else
-                cout << "Invalid Row Number2" << endl;
+                cout << "Invalid Row Number" << endl;
 
         }
 
@@ -178,5 +235,12 @@ void modifySeatingChart( char row, char seatingChart[][13] )
             cout << "Invalid Row Number1" << endl;
     }
 
+}
+
+void initSeatingChart( char seatingChart[][7] )
+{
+    for( int i = 0; i < 14; i++ )
+        for( int j = 0; j < 7; j++ )
+            seatingChart[i][j] = '*';
 }
 
