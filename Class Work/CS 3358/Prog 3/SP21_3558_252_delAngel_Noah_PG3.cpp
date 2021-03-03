5d2a5f3fc12431dc266f4415d91b0351a20e8adb
unknown <noahdelangel@gmail.com>
2021-03-02 19:39:09 -0600

    Added comment headers to be consistent with the style guide. Made some changes to program description. Also corrected error in the logic that prevented the program from going back to menu if an invalid option was given. Added logic for program to determine how much extra change the user recieves

--- Class Work/CS 3358/Prog 3/SP21_3558_252_delAngel_Noah_PG3.cpp
+++ Class Work/CS 3358/Prog 3/SP21_3558_252_delAngel_Noah_PG3.cpp
@@ -4,12 +4,83 @@
 // Spring - 2021 - CS 3358 - 252
 // Instructor:  Husain Gholoom
 //
-// Description of purpose of program
+// A simulated vending machine that
+// uses classes
 #include <iostream>
 #include <vector>
 
 using namespace std;
 
+//**************************************************
+// This class holds the cost and number of items available
+// for a particular item
+//
+// Private Variables:
+//      numOfItems - keeps track of the number of available
+//                   for that item
+//      cost - keeps track of the cost of the item
+// Public Functions:
+//      DispenserType() - default constructor initializes
+//                         numOfItems to 3
+//      returnNumOfItems() - returns the number of items for that object
+//      setCost(input) - sets the cost for the item
+//      returnCost() - returns the cost of the item
+//      reduceNumOfItems() - reduces the number of available items
+//**************************************************
+
+class DispenserType
+{
+    private:
+        int numOfItems,
+            cost;
+    public:
+        DispenserType()
+        {
+            numOfItems = 3;
+        }
+
+        int returnNumOfItems()
+        {
+            return numOfItems;
+        }
+
+        void setCost( int input )
+        {
+            cost = input;
+        }
+
+        int returnCost()
+        {
+            return cost;
+        }
+
+        void reduceNumOfItems()
+        {
+            numOfItems -= 1;
+        }
+};
+
+//**************************************************
+// This class keeps track of all the money flowing
+// in and out of the cash register
+//
+// Private Variables:
+//      cashOnHand - Holds the amount of cash that the register has
+// Public Functions:
+//      CashRegister() - default constructor sets cashOnHand to 500
+//      returnCash() - returns the cashOnHand for the register
+//      receiveCash(int receiveCash, int cost,
+//                  DispenserType &item) - Attempts to receive cash
+//                  deposited by user. If the user inputs more cash then
+//                  required the function will determine the change and tell
+//                  the user to collect their change. However, if the user
+//                  inputs less change than the request amount, it determines
+//                  how much more is required and asks the user to deposit
+//                  that amount. If the user still fails to input enough
+//                  change, end the transaction.  If the user fails to input
+//                  enough cash twice the the function returns false, otherwise
+//                  the function returns true.
+//**************************************************
 class CashRegister
 {
     private:
@@ -25,82 +96,19 @@ class CashRegister
             return cashOnHand;
         }
 
-        bool recieveCash( int recieveCash, int cost );
+        bool receiveCash( int receiveCash, int cost, DispenserType &item );
 };
 
-bool CashRegister::recieveCash( int recieveCash, int cost )
-{
-    // Variable declarations
-    bool enoughCash = true;
-    int delta = cost - recieveCash,
-        additionalCash;
-
-    // If the cash is not enough, add the cash
-    if ( recieveCash != cost )
-    {
-        cout << "Please deposit another " << delta << " cents" << endl;
-        cin >> additionalCash;
-
-        // If more cash is still not added
-        // return the program with a false bool
-        if( additionalCash != delta )
-        {
-            cout << "The amount is not enough. Collect what you deposited."
-                 << endl;
-            cout << "*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*" << endl << endl;
-
-            enoughCash = false;
-            return recieveCash;
-        }
-    }
-
-    // Once all required cash is recieved, add it to
-    // the register.
-    cashOnHand += cost;
-
-
-    return recieveCash;
-}
-
-class DispenserType
-{
-    private:
-        int numOfItems,
-            cost;
-    public:
-        Dispenser()
-        {
-            numOfItems = 3;
-        }
-
-        int returnNumOfItems()
-        {
-            return numOfItems;
-        }
-
-        int setCost( int input )
-        {
-            cost = input;
-        }
-
-        int returnCost()
-        {
-            return cost;
-        }
-
-        int reduceNumOfItems()
-        {
-            numOfItems -= 1;
-        }
-};
-
+// Displays menu, and runs input loop for program
 void menu( DispenserType[], CashRegister & );
+
+// Initializes array to basic state
 void populateMachine( DispenserType[]  );
 
 int main( )
 {
     // Variable declarations
-    DispenserType DispenserItems[3];
+    DispenserType DispenserItems[4];
     CashRegister Register;
 
     // Populate Array
@@ -112,7 +120,82 @@ int main( )
     return 0;
 }
 
-void menu( DispenserType inputItems[9], CashRegister &InputRegister )
+//**************************************************
+// Attempts to receive cash deposited by user. If the
+// user inputs more cash then required the function
+// will determine the change and tell the user to collect
+// their change. However, if the user inputs less change
+// than the request amount, it determines how much more is
+// required and asks the user to deposit that amount. If
+// the user still fails to input enough change, end the transaction.
+// If the user fails to input enough cash twice the the function
+// returns false, otherwise the function returns true.
+//
+// receiveCash - integer that holds cash the user
+// attempts to deposit
+// cost - integer that holds the cost of the item
+// item - A DispenserType Object that holds the
+//        item
+//**************************************************
+bool CashRegister::receiveCash( int receiveCash, int cost, DispenserType &item )
+{
+    // Variable declarations
+    bool enoughCash = true;
+    int delta = cost - receiveCash,
+        additionalCash = 0;
+
+    cout << endl;
+
+    // If the cash is not enough, add the cash
+    if ( receiveCash < cost )
+    {
+        cout << "Please deposit another " << delta << " cents" << endl;
+        cin >> additionalCash;
+        cout << endl;
+
+        // If more cash is still not added
+        // return the program with a false bool
+        if( additionalCash < delta )
+        {
+            cout << "The amount is not enough. Collect what you deposited."
+                 << endl;
+            cout << "*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*" << endl << endl;
+
+            enoughCash = false;
+
+            // Return success of function
+            return enoughCash;
+        }
+    }
+
+    cout << "Collect your item at the bottom and enjoy." << endl;
+
+    // If the cash is more than requested provide change
+    if( (receiveCash + additionalCash ) > cost )
+    {
+        delta = (receiveCash + additionalCash) - cost;
+
+        cout << "Collect your change " <<delta << endl;
+    }
+
+    // Once all required cash is recieved, add it to
+    // the register. And, reduce the number of items
+    cashOnHand += cost;
+    item.reduceNumOfItems();
+
+    cout << "*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*" << endl << endl;
+
+    // Return success of function
+    return enoughCash;
+}
+
+//**************************************************
+// Displays menu, and runs input loop for program
+//
+// inputItems - Holds the available items
+// inputRegister - Holds the cash register object
+//**************************************************
+void menu( DispenserType inputItems[3], CashRegister &inputRegister )
 {
     // Variable declarations
     bool continueMenu = true, correctinput = false;
@@ -130,7 +213,7 @@ void menu( DispenserType inputItems[9], CashRegister &InputRegister )
         input = 0;
         index = 0;
         cost = 0;
-        correctinput = false;
+        correctinput = true;
 
         cout << "Select One of the following" << endl << endl;
         cout << "     1 for Candy" << endl;
@@ -147,12 +230,18 @@ void menu( DispenserType inputItems[9], CashRegister &InputRegister )
             cin >> input;
 
             // if input breaks throw error
-            if( !cin )
+            if( !cin || input > 9 || input < 1)
                 throw input;
+
+            cout << endl;
         }
         catch(...)
         {
+            cout << endl;
             cout << "Invalid selection." << endl << endl;
+
+            // Set input to false
+            correctinput = false;
 
             // Clear items in the cin
             cin.clear();
@@ -183,43 +272,45 @@ void menu( DispenserType inputItems[9], CashRegister &InputRegister )
                 break;
         }
 
-        if( inputItems[index].returnNumOfItems() == 0 )
-        {
-            cout << "Sorry, this item is sold out." << endl;
-            cout << "*-*-*-*-*-*-*-*-*-*-*-*-*-*-*" << endl;
-        }
-        else
-        {
-            // Determine cost
-            cost = inputItems[index].returnCost();
+        // If the user wishes to continue get money
+        // for the item they want
+        if( continueMenu == true && correctinput == true)
 
-            cout << "Please deposit " << cost << " cents" << endl;
-
-            while( correctinput == false )
+            // If there is still items
+            if( inputItems[index].returnNumOfItems() == 0 )
             {
-                try
-                {
-                    cin >> deposit;
-                    // if input breaks throw error
-                    if( !cin )
-                        throw input;
+                cout << "Sorry, this item is sold out." << endl;
+                cout << "*-*-*-*-*-*-*-*-*-*-*-*-*-*-*" << endl << endl;
+            }
+            else
+            {
+                // Determine cost
+                cost = inputItems[index].returnCost();
 
-                    correctinput = true;
-                }
-                catch(...)
-                {
-                    cout << "Please enter an integer" << endl << endl;
+                cout << "Please deposit " << cost << " cents" << endl;
 
-                    // Clear items in the cin
-                    cin.clear();
-                    cin.ignore(256, '\n');
-                }
+
+                    try
+                    {
+                        cin >> deposit;
+                        // if input breaks throw error
+                        if( !cin )
+                            throw input;
+
+                        inputRegister.receiveCash( deposit, cost,
+                                                   inputItems[index] );
+
+                    }
+                    catch(...)
+                    {
+                        cout << endl;
+                        // Clear items in the cin
+                        cin.clear();
+                        cin.ignore(256, '\n');
+                    }
+
 
             }
-
-            InputRegister.recieveCash( deposit, cost );
-
-        }
     }
 
     // Footer statement
@@ -227,7 +318,12 @@ void menu( DispenserType inputItems[9], CashRegister &InputRegister )
          << endl;
 }
 
-void populateMachine( DispenserType inputItems[9] )
+//**************************************************
+// Initializes vending machine objects cost
+//
+// inputItems - Holds the available items
+//**************************************************
+void populateMachine( DispenserType inputItems[3] )
 {
     // Initialize array
     for( int i = 0; i < 4; i++ )
