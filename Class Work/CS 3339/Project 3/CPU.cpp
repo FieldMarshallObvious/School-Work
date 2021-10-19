4184390f33906636097d3edba74a5b8243c63c7e
FieldMarshallObvious <noahdelangel@gmail.com>
2021-10-19 18:31:54 -0500

    Made some formatting changes, and added memOps. Also tried to correct bubbles, and it seems closer but not quite there, as the num of bubbles is still off

--- Class Work/CS 3339/Project 3/CPU.cpp
+++ Class Work/CS 3339/Project 3/CPU.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cbfd499f8b4e2203fb4e84c1dac2134c15d6c7ec4b8cf9dd63b1a77743a90705
-size 11634
+oid sha256:b7ad91acc53b0f6b868b46bd0d2c23106ea59241dcc061492ff75f27016d5394
+size 12108
