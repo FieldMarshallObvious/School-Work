c93d387a6a070b3bf54ecb4b9400cadbeb404174
unknown <noahdelangel@gmail.com>
2022-01-29 18:17:03 -0600

    Complete pizza queue

--- Class Work/CS 3354/Pizza Queue/pizzaQueue.java
+++ Class Work/CS 3354/Pizza Queue/pizzaQueue.java
@@ -4,84 +4,64 @@ public class pizzaQueue
     public static void main(String[] args) 
     {
         // Create objects for first and last objects 
-        pizza queue = new pizza(); pizza first = new pizza(); pizza last = new pizza();
+        pizzaList queue = new pizzaList();
 
-        queue.enqueue( first, last, new pizza("pepperoni", "1234 Bobcat Trail"));
-        queue.enqueue(first, last, new pizza("sausage", "2345 University Drive"));
+        queue.enqueue( new pizza("pepperoni", "1234 Bobcat Trail"));
+        queue.enqueue(new pizza("sausage", "2345 University Drive"));
 
-        queue.deliver(first, last);
+        queue.deliver();
 
-        queue.enqueue(first, last, new pizza("extra cheese", "3456 Rickster Road"));
-        queue.enqueue(first, last, new pizza("everything", "4567 King Court"));
-        queue.enqueue(first, last, new pizza("coffee beans", "5678 Java Circle"));
+        queue.enqueue( new pizza("extra cheese", "3456 Rickster Road"));
+        queue.enqueue(new pizza("everything", "4567 King Court"));
+        queue.enqueue( new pizza("coffee beans", "5678 Java Circle"));
 
-        queue.deliver(first, last);
-        queue.deliver(first, last);
-        queue.deliver(first, last);
-        queue.deliver(first, last);
-        queue.deliver(first, last);
+        queue.deliver();
+        queue.deliver();
+        queue.deliver();
+        queue.deliver();
+        queue.deliver();
 
     }
 
 }
 
-class pizza 
+class pizzaList 
 {
-    // Variable declarations
-    public String ingredients, address;
-    public pizza next;
+    // Head and object declarations 
+    private pizza head;
+    private pizza tail;
 
-    // default initializer 
-    public pizza( )
+    // Initializer with head and tail
+    public pizzaList( )
     {
-        ingredients = " ";
-        address = " ";
-        next = null;
-    }
-
-    // Object initializer with parameters
-    public pizza( String inputIngredients, String inputAddress ) 
-    {
-        ingredients = inputIngredients;
-        address = inputAddress;
+        head = new pizza();
+        tail = new pizza();
     }
 
     // Add an item to the queue 
-    public void enqueue( pizza head, pizza tail, pizza thispizza )
+    public void enqueue( pizza thispizza )
     {
         // If there is no head, create one 
         if ( head.ingredients == " " && head.address == " " ) 
         {
-            // modify individual items 
-            head.ingredients = thispizza.ingredients;
-            head.address = thispizza.address;
-            head.next = thispizza;
+            this.head = thispizza;
         }
         // If there already is a queue append this item to the end of the list 
-        else tail.next = new pizza( thispizza.ingredients, thispizza.address);
-        tail = thispizza;
+        else this.tail.next = thispizza;
+        this.tail = thispizza;
     }
 
     // Remove an itme from the queue 
-    public pizza dequeue( pizza head, pizza tail )
+    public pizza dequeue(  )
     {
         // Variable declarations
-        pizza pizzaToDeliver = new pizza(); // Keep track of the pizza to be delivied 
+        pizza pizzaToDeliver = null; // Keep track of the pizza to be delivied 
         
         // If the list is exists then reduce the head by one 
         if ( head != null )
         {
-            // Get the pizza data to the pizza delivery 
-            pizzaToDeliver.ingredients = head.ingredients;
-            pizzaToDeliver.address = head.address;
-            
-            if( head.next == null )
-            {
-                // Set the head to next 
-                head.ingredients = head.next.ingredients;
-                head.address = head.next.address;
-                head.next = head.next.next;
-            }
+            pizzaToDeliver = head;
+            head = head.next;
         }
 
         // If there is no items in the list then ensure the tail is gone
@@ -92,10 +72,10 @@ class pizza
     }
 
     // deliver pizzas from the queue 
-    public void deliver( pizza head, pizza tail )
+    public void deliver( )
     {
         // Variable declarations
-        pizza thisPizza = dequeue(head, tail);
+        pizza thisPizza = dequeue( );
 
         // If there is no pizza to be delivered
         if( thisPizza == null )
@@ -107,4 +87,26 @@ class pizza
         // Otherwise print the pizza and the address
         System.out.print("Deliver a pizza with " + thisPizza.ingredients + " to " + thisPizza.address + "\n");
     }
+}
+
+class pizza
+{
+        // Variable declarations
+        public String ingredients, address;
+        public pizza next;
+
+        public pizza()
+        {
+            ingredients = " ";
+            address = " ";
+            next = null;
+        }
+
+        // Object initializer with parameters
+        public pizza( String inputIngredients, String inputAddress ) 
+        {
+            ingredients = inputIngredients;
+            address = inputAddress;
+            next = null;
+        }
 }
