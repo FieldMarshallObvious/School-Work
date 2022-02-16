03ab52780f53b6f77c5c868b60ccc252a9ed6f16
FieldMarshallObvious <noahdelangel@gmail.com>
2022-02-16 10:54:12 -0600

    Completed command line program! Also removed debug statements for command line program. Recreated the zip file for the changes

--- Class Work/CS 4328/Project 1/commandLine.c
+++ Class Work/CS 4328/Project 1/commandLine.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <sys/types.h>
+#include<sys/wait.h>
 
 
 int main ( int argc, char *argv[] )
@@ -8,13 +9,14 @@ int main ( int argc, char *argv[] )
     // Create fork
     int pid = fork();
 
+    int status;
+
     if ( argc < 1 )
     {
         printf("One argument expected.\n");
         return(-1);
     }
 
-    argv[argc - 1] = NULL;
 
     // An error has occured 
     if( pid < 0 )
@@ -24,15 +26,24 @@ int main ( int argc, char *argv[] )
     }
 
     else if( pid == 0 )
-    {
-
-        execvp(argv[0], argv);
+    {   
         printf("Child proccess id is: %d\n", getpid());
+
+        // In case of error return a negative statement
+        if(execvp(argv[1], argv + 1) < 0 )
+        {
+            printf("Command failed\n");
+            return -2;
+        }
     }
 
     else
-    {        
+    {   
+        // Wait for the child proccess to complete
+        while(wait(&status) != pid);
+
         printf("Parent proccess id is: %d\n", getppid());
+
     }
 
     return 0;
