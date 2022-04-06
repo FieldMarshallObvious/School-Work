16b40084912eb2730342f48a3791f0fd9f471ed6
FieldMarshallObvious <noahdelangel@gmail.com>
2022-04-06 13:21:00 -0500

    Actually completed assignment

--- Class Work/CS 4328/Project 3/proj3B.c
+++ Class Work/CS 4328/Project 3/proj3B.c
@@ -48,28 +48,28 @@ void *APrinter()
     {
         // If the buffer is full 
         // then wait
-        if (buffer.size == CAPACITY)
+        while (buffer.size > 0)
         {
             pthread_cond_wait(&full_cond, &mutex);
 
         }
-        else
+
+        pthread_mutex_lock(&mutex);
+        for( int j = 0; j < 5; j++)
         {
-            pthread_mutex_lock(&mutex);
-            for( int j = 0; j < 5; j++)
-            {
-                printf("%d: AAAAA\n", completedItems);
-            }
-            // Keep track of the number of completed items 
-            completedItems++;
-
-            // Open the buffer
-            buffer.size++;
-
-            printf("\n");
-
-            pthread_cond_broadcast(&empty_cond);
+            printf("%d: AAAAA\n", completedItems);
         }
+        // Keep track of the number of completed items 
+        completedItems++;
+
+        // Open the buffer
+        buffer.size++;
+
+        printf("\n");
+
+        pthread_cond_broadcast(&empty_cond);
+        pthread_mutex_unlock(&mutex);
+
     }
 }
 
@@ -80,26 +80,24 @@ void *BPrinter()
     // print B(x5) 10 times
     while(completedItems != ITEMSIZE)
     {
-        if ( buffer.size == 0 )
+        while ( buffer.size < CAPACITY )
         {
             pthread_cond_wait(&empty_cond, &mutex);
         }
-        else 
-        {
-            pthread_mutex_lock(&mutex);
-            for( int j = 0; j < 5; j++)
-            {
-                printf("%d: BBBBB\n", completedItems);
-            }
-            // Keep track of the number of completed items 
-            completedItems++;
 
-            // Return the buffer to the A printer 
-            buffer.size--;
-            printf("\n");
-            
-            pthread_cond_broadcast(&full_cond);
-            pthread_mutex_unlock(&mutex);
+        pthread_mutex_lock(&mutex);
+        for( int j = 0; j < 5; j++)
+        {
+            printf("%d: BBBBB\n", completedItems);
         }
+        // Keep track of the number of completed items 
+        completedItems++;
+
+        // Return the buffer to the A printer 
+        buffer.size--;
+        printf("\n");
+        
+        pthread_cond_broadcast(&full_cond);
+        pthread_mutex_unlock(&mutex);
     }
 }
