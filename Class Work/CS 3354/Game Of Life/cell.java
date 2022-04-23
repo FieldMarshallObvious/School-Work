61cc875b9bf28b220ef0b2afbd7c9bf05a10ac8f
FieldMarshallObvious <noahdelangel@gmail.com>
2022-04-23 15:52:09 -0500

    Correct logical errors in game of life program

---
+++ Class Work/CS 3354/Game Of Life/cell.java
@@ -0,0 +1,13 @@
+public class cell {
+    public boolean isAlive;
+
+    public cell( )
+    {
+        isAlive = false;
+    }
+
+    public cell ( boolean aliveCondition )
+    {
+        isAlive = aliveCondition;
+    }
+}
