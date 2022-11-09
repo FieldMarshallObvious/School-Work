e2d3a2dc6a83b7d8317bfefa0d91937a64629c0f
unknown <noahdelangel@gmail.com>
2022-11-09 09:30:19 -0600

    Corrected color wheel & added button functionality

--- Personal Projects/Connor's Light Cube/LightCubeRunner/LightCubeRunner.ino
+++ Personal Projects/Connor's Light Cube/LightCubeRunner/LightCubeRunner.ino
@@ -18,21 +18,24 @@
 
 Adafruit_NeoPixel pixels(NUMPIXELS, RGBRINPIN, NEO_GRB + NEO_KHZ800);
 
-// Keep track of if the button has been
-// pressed
-bool curButtonState = false;
+
+// Handle debouncing
+int lastButtonState = 0;
+unsigned long lastDebounceTime = 0;
+unsigned long debounceDelay = 1;
+int buttonState;
 
 bool colorsInitialized = false;
 
 int colorSet1[4][3] = { {30, 203, 225}, {150, 30, 225}, {225, 52, 30}, {106, 225, 30}};
-  int curPixelColorIndex[16] = {-1, -1, -1, -1, -1, -1, -1, -1, -1 ,-1, -1, -1, -1, -1, -1, -1};
+int curPixelColorIndex[16] = {-1, -1, -1, -1, -1, -1, -1, -1, -1 ,-1, -1, -1, -1, -1, -1, -1};
+int counter = 0;
 
 void setup() {
-  Serial.begin(115200);
-
+  Serial.begin(9600);
   // setup button pins
   pinMode(BUTTONPINSET1, INPUT);
-  pinMode(BUTTONPINSET1, INPUT);
+  pinMode(BUTTONPINSET2, INPUT);
 
   // setup led pins
   pinMode(RGBRINPIN, OUTPUT);
@@ -43,7 +46,32 @@ void setup() {
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  int reading = digitalRead(BUTTONPINSET1);
+  //int buttonState2 = digitalRead(BUTTONPINSET2);
+
+  // If the siwtch changed due to noise
+  // log the debounce time
+  if( reading != lastButtonState )
+  {
+    lastDebounceTime = millis();
+  }
+
+  if( (millis() - lastDebounceTime) > debounceDelay )
+  {
+    if( reading != buttonState )
+    {
+      buttonState = reading;      
+      if( buttonState == 0 )
+      {
+        Serial.println("Button pressed!");
+        colorsInitialized = false;
+        resetCurColorArray();
+      }
+      else 
+        Serial.println("Button is not pressed!");  
+    }
+  }  
+  
   pixels.clear();
   pixels.setBrightness(10);
 
@@ -51,6 +79,29 @@ void loop() {
   // colors if they are not initialized
   if (!colorsInitialized)
     initializeColors(colorSet1);
+  else
+    cycleColors(colorSet1, curPixelColorIndex, counter); 
+  
+  if ( counter < 4 )
+  {
+    counter++;
+  }
+  else 
+  {
+    counter = 0;
+  }
+  
+  lastButtonState = reading;
+
+}
+
+// Reset all items in the color array
+
+void resetCurColorArray() {
+  for( int i = 0; i < 16; i++ )
+  {
+    curPixelColorIndex[i] = -1;    
+  }
 }
 
 void initializeColors(int colorSet[][3]) {
@@ -82,11 +133,11 @@ void initializeColors(int colorSet[][3]) {
       }
     }
 
-    Serial.print("Pixel array is ");
+    /*Serial.print("Pixel array is ");
     Serial.print(curPixelColorIndex[i], DEC);
     Serial.print(" @ ");
-    Serial.println(i, DEC);
-
+    Serial.println(i, DEC);*/
+    
     // Set the current pixel 
     pixels.setPixelColor(i, pixels.Color(colorSet[curPixelColorIndex[i]][0], colorSet[curPixelColorIndex[i]][1], colorSet[curPixelColorIndex[i]][2]));
   }
@@ -94,32 +145,42 @@ void initializeColors(int colorSet[][3]) {
 
   Serial.println("Showing pixels!");
 
-  delay(500);
+  delay(250);
 
   colorsInitialized = true;
 }
 
-// Iterate each of the four leds
+// Change each of the four leds to their next
+// neighboor
 void cycleColors(int colorSet[][3], int curPixelColorIndex[], int counter) 
 {
   changeItem(counter, curPixelColorIndex);
-  pixels.setPixelColor(counter, pixels.Color(colorSet[curPixelColorIndex[counter]][0], colorSet[curPixelColorIndex[counter]][1], colorSet[curPixelColorIndex[counter]][2]));
+  changeItem(counter + 4, curPixelColorIndex);
+  changeItem(counter + 8, curPixelColorIndex);
+  changeItem(counter + 12, curPixelColorIndex);
+
+  for( int i = 0; i < 16; i++ )
+  {
+    pixels.setPixelColor(i, pixels.Color(colorSet[curPixelColorIndex[i]][0], colorSet[curPixelColorIndex[i]][1], colorSet[curPixelColorIndex[i]][2]));
+  }
   pixels.show();
+  delay(250);
+} 
 
-}
-
-
-
+// Swap the desired item with it's neighboor
 void changeItem(int counter,  int * curPixelColorIndex) {
-    // Decrease this items value if it is greater than 0
-    if( curPixelColorIndex[counter] > 0 )
-    {
-      curPixelColorIndex[counter] -= 1;
-    }
-    else
-    {
-      curPixelColorIndex[counter] = 3;
-    }
+  // If there is another value after this one
+  // set this value to the next value
+  if( counter < 15 )
+  {
+    curPixelColorIndex[counter] = curPixelColorIndex[counter + 1];    
+  }
+  // Otherwise set it to the first 
+  // item in the array
+  else 
+  {
+    curPixelColorIndex[counter] = curPixelColorIndex[0];
+  }
 }
 
 
